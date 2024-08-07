affb3f68e3d535fb0e064f415bdff562f3ab6ffd
Konstantin Hoffie <donkon@localhost.localdomain>
2024-08-07 13:53:55 +0200

    pop / 1e3, age_groups recoded

--- gen_data.R
+++ gen_data.R
@@ -4,8 +4,11 @@ library(MigStat)
 age_for <- fread(file.path(ps$clean, "aux_data", "age17for.csv"))
 flows <- ana$flows[year == 2017, .(fromdist = origin, todist = destination,
                                    year, agegroup = age_group, dist = distance_pos,
-                                   flows = flow, frompop = pop_o, topop = pop_d)]
-flows[age_for, frompop_ger := i.german, on = .(fromdist = region, year, agegroup = age_group)]
-flows[age_for, topop_ger := i.german, on = .(todist = region, year, agegroup = age_group)]
+                                   flows = flow, frompop = pop_o / 1e3, topop = pop_d / 1e3)]
+flows[age_for, frompop_ger := i.german / 1e3, on = .(fromdist = region, year, agegroup = age_group)]
+flows[age_for, topop_ger := i.german / 1e3, on = .(todist = region, year, agegroup = age_group)]
 
-fwrite(flows, "~/Documents/GermanMigration/FlowDataGermans.csv")
+flows[agegroup == "über65", agegroup := "above65"]
+flows[agegroup == "unter18", agegroup := "below18"]
+flows <- flows[agegroup != "all"]
+fwrite(flows, "~/Documents/GermanMigration/data/FlowDataGermans.csv")
