e1b675d02b512c5fd11452fbe30bf25cde7568d7
Konstantin Hoffie <donkon@localhost.localdomain>
2024-08-07 14:16:57 +0200

    def model2 after including model2.jl, deleted vi code

--- fitmodel2.jl
+++ fitmodel2.jl
@@ -1,15 +1,14 @@
 includet("model2.jl")
+model2 = migration2(dt2.flows, levelcode.(dt2.fromdist), levelcode.(dt2.todist),
+                    dt2.frompop, dt2.topop, dt2.distance,
+                    levelcode.(dt2.agegroup), Nages, Ndist, meddist, netactual)
+
 netactual = calcnet(dt2.flows,
                     levelcode.(dt2.fromdist),
                     levelcode.(dt2.todist),
                     levelcode.(dt2.agegroup),Nages,Ndist)
 
-model2 = migration2(dt2.flows, levelcode.(dt2.fromdist), levelcode.(dt2.todist),
-                    dt2.frompop, dt2.topop, dt2.distance,
-                    levelcode.(dt2.agegroup), Nages, Ndist, meddist, netactual)
 
-
-model2
 opinit = [fill(11.0,Nages); fill(3.3,Nages); fill(1.8,Nages); fill(.1,Nages); [1]; 1 .* 100 .* ones(Ndist*Nages)]
 
 opinit = [  [.5, 4.5, 5.7, .53, .2, .14];
@@ -22,10 +21,6 @@ opinit = [  [.5, 4.5, 5.7, .53, .2, .14];
 lower = [fill(0,Nages); fill(0,Nages); fill(0,Nages); fill(0,Nages); [0]; .7 .* 100 .* ones(Ndist*Nages)]
 upper = [fill(20,Nages); fill(20,Nages); fill(5,Nages); fill(1,Nages); [2]; 1.3 .* 100 .* ones(Ndist*Nages)]
 
-# appx = vi(model2, ADVI(10, 1000))
-# rand(appx, 1000)
-## use optimization to find a good fit
-##lb = lower, ub = upper,
 mapfit2 = maximum_a_posteriori(model2, LBFGS() ; adtype = AutoReverseDiff(), 
                                initial_params = opinit, lb = lower, ub = upper,
                                maxiters = 200, maxtime = 60, reltol = .008)
