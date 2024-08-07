ed07e43216575161cb8371e2e9e94f19eb9a56f7
Konstantin Hoffie <donkon@localhost.localdomain>
2024-08-07 13:33:44 +0200

    commented out some lines

--- fitturing.jl
+++ fitturing.jl
@@ -17,13 +17,13 @@ if sims
     dt,knowndesir = simdatafromtemplate(Xoshiro(20240725),"data/flowtemplate.csv")
     rename!(dt,Dict(:age_group => :agegroup,:distance => :dist,:flow => :flows))
 else
-    dt = CSV.read("/home/donkon/Diss/inst/extdata/clean/daniel/FlowData.csv",DataFrame)
+    dt = CSV.read("./data/FlowDataGermans.csv",DataFrame)
 end
 dt2 = dt
-dt2 = dt[dt[: , 6] .> 10, :]
-random40 = selectdists(munis, 50)[11:50]
-top40 = selectdists(munis, 400)[1:100]
-dt2 = subsetdists(dt, top40)
+# dt2 = dt[dt[: , 6] .> 10, :]
+# random40 = selectdists(munis, 50)[11:50]
+# top40 = selectdists(munis, 400)[1:100]
+# dt2 = subsetdists(dt, top40)
 
 ## top10 from R: 11000  2000  9162  3241  5315  6412  8111  5111  5562  5382
 dt2.fromdist = categorical(dt2.fromdist)
@@ -31,7 +31,7 @@ dt2.todist = categorical(dt2.todist)
 dt2.agegroup = categorical(dt2.agegroup)
 levels!(dt2.agegroup,["below18","18-25","25-30","30-50","50-65","above65"])
 rename!(dt2, Dict(:dist => :distance))
-scatter(dt2.distance, log.(dt2.flows ./ dt2.frompop ./ dt2.topop))
+## scatter(dt2.distance, log.(dt2.flows ./ dt2.frompop ./ dt2.topop))
 
 meddist = median(dt2.distance)
 Ndist = length(unique(dt2.fromdist))
