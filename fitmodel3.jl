185fe92452884b9722ae9a46c5793246e1be5131
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-08-08 11:32:13 +0200

    swapped some lines

--- fitmodel3.jl
+++ fitmodel3.jl
@@ -1,26 +1,29 @@
 includet("model3.jl")
 optis = CSV.read("./data/opti_d0.csv", DataFrame)
 
-dt2 = dt
-dt2 = dt[dt[: , 6] .> 10, :]
-
 ncoefs = 64
+
+opinit = [optis[:, 2]; [1.5]; 0 * ones(ncoefs)]
+lower = [fill(0,Nages); fill(0,Nages); fill(0,Nages); fill(0,Nages); [.05]; -4 * ones(ncoefs)]
+upper = [fill(20,Nages); fill(10,Nages); fill(5,Nages); fill(1,Nages); [2]; 4 * ones(ncoefs)]
+
+dt2 = dt
+
+dt2 = dt[dt[: , 6] .> 1, :]
 netactual = calcnet(dt2.flows,
                     levelcode.(dt2.fromdist),
                     levelcode.(dt2.todist),
                     levelcode.(dt2.agegroup),Nages,Ndist)
 
 model3 = migration3(dt2.flows, levelcode.(dt2.fromdist), levelcode.(dt2.todist),
-                    dt2.frompop, dt2.topop, dt2.distance,
+                    dt2.frompop_ger, dt2.topop, dt2.distance,
                     levelcode.(dt2.agegroup), Nages,
-                    coords.xcoord, coords.ycoord,
+                    coords_dt.xcoord, coords_dt.ycoord,
                     Ndist, meddist, netactual, ncoefs)
 
-opinit = [optis[:, 2]; [1]; 1 * ones(ncoefs)]
-lower = [fill(0,Nages); fill(0,Nages); fill(0,Nages); fill(0,Nages); [0]; -4 * ones(ncoefs)]
-upper = [fill(20,Nages); fill(20,Nages); fill(5,Nages); fill(1,Nages); [2]; 4 * ones(ncoefs)]
-
+dt2
 mapfit3 = maximum_a_posteriori(model3, LBFGS() ; adtype = AutoReverseDiff(), 
                                initial_params = opinit, lb = lower, ub = upper,
                                maxiters = 20, maxtime = 60, reltol = .08)
 
+mapfit3.values
