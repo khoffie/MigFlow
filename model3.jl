d109a5eeb8c50b8fe19b8fa00d70212176420aea
Daniel Lakeland <dlakelan@street-artists.org>
2024-08-08 08:21:28 -0700

    add age groups to the polynomial

--- model3.jl
+++ model3.jl
@@ -12,18 +12,19 @@ using Printf, ApproxFun
     neterr ~ Gamma(3.0, 2.0/2.0)
 
     
-    desirecoefs ~ MvNormal(zeros(ncoefs),ones(ncoefs)) 
-    desfun = Fun(Chebyshev(200000.0 .. 600000.0) * Chebyshev(5e6 .. 6.2e6), desirecoefs)
+    desirecoefs ~ MvNormal(zeros(ncoefs*Nages),20.0 .* ones(ncoefs*Nages)) 
+    desirecoefsre = reshape(desirecoefs,(ncoefs,Nages))
+    desfuns = [Fun(Chebyshev(200000.0 .. 600000.0) * Chebyshev(5e6 .. 6.2e6), desirecoefsre[:,i]) for i in 1:Nages]
     
-    desires = [exp(desfun(xcoord[todist[i]],ycoord[todist[i]]) -
-        desfun(xcoord[fromdist[i]],ycoord[fromdist[i]]))
+    desires = [exp(desfuns[agegroup[i]](xcoord[todist[i]],ycoord[todist[i]]) -
+        desfun[agegroup[i]](xcoord[fromdist[i]],ycoord[fromdist[i]]))
                for i in 1:length(flows)]
     preds = [frompop[i] * topop[i] * a[agegroup[i]] / 1000.0 *
         (1.0 + b[agegroup[i]] / (distance[i] / meddist + d0[agegroup[i]])^c[agegroup[i]]) * desires[i]
              for i in 1:length(flows)]
 
     if typeof(a[1]) != Float64
-        @printf "a1 = %.2f, b1 = %.2f, c1 = %.2f, d0_1 = %.2f, neterr = %.2f", a[1].value,b[1].value,c[1].value,d0[1].value,neterr.value
+        @printf "a1 = %.2f, b1 = %.2f, c1 = %.2f, d0_1 = %.2f, neterr = %.2f"  a[1].value b[1].value c[1].value d0[1].value neterr.value
     end
     if any(isnan,preds)
         println("NaN in predictions")
