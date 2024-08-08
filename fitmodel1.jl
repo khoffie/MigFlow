89a2165b9dbf0cc9ce00fc2c2e20b82024e578b7
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-08-08 09:52:31 +0200

    deleted sampling of migration1old

--- fitmodel1.jl
+++ fitmodel1.jl
@@ -1,36 +1,28 @@
 includet("model1.jl")
+optis = CSV.read("./data/opti_d0.csv", DataFrame)
 
-### one age group at a time
-dt2 = dt[dt.agegroup .== "below18", :]
-dt2 = dt
-model1 = migration1old(dt2.flows, levelcode.(dt2.fromdist), levelcode.(dt2.todist),
-                    dt2.frompop_ger, dt2.topop, dt2.distance,
-                    levelcode.(dt2.agegroup), Nages, Ndist, meddist)
-opinit = [10; 3; 2; .1]
-lower = [0; 0; 0; 0]
-upper = [30; 10; 5; 1]
-mapfit1 = maximum_a_posteriori(model1, LBFGS() ; adtype = AutoReverseDiff(), 
-                               initial_params = opinit, lb = lower, ub = upper,
-                               maxiters = 20, maxtime = 60, reltol = .08)
-mapfit1
-initvals = mapfit1.values
-
-
-## all age groups together
-model1 = migration1new(dt2.flows, levelcode.(dt2.fromdist), levelcode.(dt2.todist),
-                    dt2.frompop_ger, dt2.topop, dt2.distance,
+model1 = migration1(dt2.flows, levelcode.(dt2.fromdist), levelcode.(dt2.todist),
+                    dt2.frompop, dt2.topop, dt2.distance,
                     levelcode.(dt2.agegroup), Nages, Ndist, meddist)
 
 opinit = [fill(11.0,Nages); fill(3.3,Nages); fill(1.8,Nages); fill(.1,Nages)]
+opinit = optis[: , 2]
 lower = [fill(0,Nages); fill(0,Nages); fill(0,Nages); fill(0,Nages)]
 upper = [fill(20,Nages); fill(10,Nages); fill(5,Nages); fill(1,Nages)]
 
 mapfit1 = maximum_a_posteriori(model1, LBFGS() ; adtype = AutoReverseDiff(), 
                                initial_params = opinit, lb = lower, ub = upper,
-                               maxiters = 200, maxtime = 600, reltol = .08)
+                               maxiters = 20, maxtime = 60, reltol = .08)
 
-mapfit1
+mapfit1.optim_result
 
 opti_params = DataFrame(names=names(mapfit1.values, 1), values=mapfit1.values.array)
 CSV.write("./data/opti_d0.csv", opti_params)
 serialize("./data/preds.csv", mapfit1)
+
+model1_chain = Chains([optis[: , 2]], optis[: , 1]) 
+dt2[:, "preds"] = generated_quantities(model1, model1_chain)[1]
+
+
+CSV.write("./data/FlowDataPreds.csv", dt2)
+CSV.write("/home/konstantin/Diss/inst/extdata/clean/daniel/FlowDataPreds.csv", dt2)
