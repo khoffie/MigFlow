c74e76f2f5cb31784cc681970c3f6be2ee86aedd
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-08-08 18:49:50 +0200

    changed inits and bounds

--- fitmodel3.jl
+++ fitmodel3.jl
@@ -3,17 +3,15 @@ optis = CSV.read("./data/opti_d0.csv", DataFrame)
 
 ncoefs = 64
 
-age = "18-25"
 
-
-opinit = [optis[:, 2]; [1.5]; 0 * ones(ncoefs)]
-opinit = optis170krows
-lower = [fill(0,Nages); fill(0,Nages); fill(0,Nages); fill(0,Nages); [.05]; -4 * ones(ncoefs)]
-upper = [fill(20,Nages); fill(10,Nages); fill(5,Nages); fill(1,Nages); [2]; 4 * ones(ncoefs)]
+opinit = [optis[:, 2]; [1.5]; 0 * ones(ncoefs * Nages)]
+opinit = [optis[:, 2]; [1.5]; rand(Normal(0.0, .4),Nages*ncoefs)]
+lower = [fill(0,Nages); fill(0,Nages); fill(0,Nages); fill(0,Nages); [.05]; -40 * ones(ncoefs * Nages)]
+upper = [fill(20,Nages); fill(10,Nages); fill(5,Nages); fill(1,Nages); [2]; 40 * ones(ncoefs * Nages)]
 
 dt2 = dt
 ## dt2 = dt[dt.agegroup .== age, :]
-dt2 = dt[dt.flows .> 1, :]
+dt2 = dt[dt.flows .> 0, :]
 meddist = median(dt2.distance)
 Ndist = length(unique(dt2.fromdist))
 Nages = length(unique(dt2.agegroup))
@@ -23,30 +21,29 @@ netactual = calcnet(dt2.flows,
                     levelcode.(dt2.fromdist),
                     levelcode.(dt2.todist),
                     levelcode.(dt2.agegroup),
-##                    ones(Int64, nrow(dt2)),
                     Nages,
                     Ndist)
 
 model3 = migration3(dt2.flows, levelcode.(dt2.fromdist), levelcode.(dt2.todist),
                     dt2.frompop_ger, dt2.topop, dt2.distance,
                     levelcode.(dt2.agegroup),
-                    # ones(Int64, nrow(dt2)),
                     Nages,
                     coords_dt.xcoord, coords_dt.ycoord,
                     Ndist, meddist, netactual, ncoefs)
-dt2
 mapfit3 = maximum_a_posteriori(model3, LBFGS() ; adtype = AutoReverseDiff(), 
                                initial_params = opinit, lb = lower, ub = upper,
-                               maxiters = 200, maxtime = 600, reltol = .08)
+                               maxiters = 20, maxtime = 60, reltol = .08)
 
-optis170krows = mapfit3.values
+opts3 = DataFrame(names=names(mapfit3.values, 1), values=mapfit3.values.array, inits = opinit)
 
-optis_3 = DataFrame(names=names(mapfit3.values, 1), values=mapfit3.values.array)
 
-model3_chain = Chains([optis_3[: , 2]], optis_3[: , 1])
-preds3 = generated_quantities(model3, model3_chain)
+density(opts3.values .- opts3.inits)
+
+opinit
+model3_chain = Chains([opts3[: , 2]], opts3[: , 1])
 dt2[:, "preds3"] = generated_quantities(model3, model3_chain)[1][1]
 
 CSV.write("./data/opti_model3.csv", optis_3)
 CSV.write("./data/FlowDataPreds3.csv", dt2)
 
+dt2
