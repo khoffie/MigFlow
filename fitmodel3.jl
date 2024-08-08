3874b09c3fc2826e1a10a37fb6de0e2d4b53fc2b
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-08-08 17:27:07 +0200

    tried model3 map, moved intermediate data creation from fitturing

--- fitmodel3.jl
+++ fitmodel3.jl
@@ -3,27 +3,50 @@ optis = CSV.read("./data/opti_d0.csv", DataFrame)
 
 ncoefs = 64
 
+age = "18-25"
+
+
 opinit = [optis[:, 2]; [1.5]; 0 * ones(ncoefs)]
+opinit = optis170krows
 lower = [fill(0,Nages); fill(0,Nages); fill(0,Nages); fill(0,Nages); [.05]; -4 * ones(ncoefs)]
 upper = [fill(20,Nages); fill(10,Nages); fill(5,Nages); fill(1,Nages); [2]; 4 * ones(ncoefs)]
 
 dt2 = dt
+## dt2 = dt[dt.agegroup .== age, :]
+dt2 = dt[dt.flows .> 1, :]
+meddist = median(dt2.distance)
+Ndist = length(unique(dt2.fromdist))
+Nages = length(unique(dt2.agegroup))
 
-dt2 = dt[dt[: , 6] .> 1, :]
+
 netactual = calcnet(dt2.flows,
                     levelcode.(dt2.fromdist),
                     levelcode.(dt2.todist),
-                    levelcode.(dt2.agegroup),Nages,Ndist)
+                    levelcode.(dt2.agegroup),
+##                    ones(Int64, nrow(dt2)),
+                    Nages,
+                    Ndist)
 
 model3 = migration3(dt2.flows, levelcode.(dt2.fromdist), levelcode.(dt2.todist),
                     dt2.frompop_ger, dt2.topop, dt2.distance,
-                    levelcode.(dt2.agegroup), Nages,
+                    levelcode.(dt2.agegroup),
+                    # ones(Int64, nrow(dt2)),
+                    Nages,
                     coords_dt.xcoord, coords_dt.ycoord,
                     Ndist, meddist, netactual, ncoefs)
-
 dt2
 mapfit3 = maximum_a_posteriori(model3, LBFGS() ; adtype = AutoReverseDiff(), 
                                initial_params = opinit, lb = lower, ub = upper,
-                               maxiters = 20, maxtime = 60, reltol = .08)
+                               maxiters = 200, maxtime = 600, reltol = .08)
 
-mapfit3.values
+optis170krows = mapfit3.values
+
+optis_3 = DataFrame(names=names(mapfit3.values, 1), values=mapfit3.values.array)
+
+model3_chain = Chains([optis_3[: , 2]], optis_3[: , 1])
+preds3 = generated_quantities(model3, model3_chain)
+dt2[:, "preds3"] = generated_quantities(model3, model3_chain)[1][1]
+
+CSV.write("./data/opti_model3.csv", optis_3)
+CSV.write("./data/FlowDataPreds3.csv", dt2)
+
