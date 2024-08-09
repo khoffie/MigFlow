4230cc8bcb327ddd07b2892b5de237c689dac918
Daniel Lakeland <dlakelan@street-artists.org>
2024-08-09 14:56:33 -0700

    modify model3 to use logistic map

--- model3.jl
+++ model3.jl
@@ -1,14 +1,15 @@
 @model function migration3(flows, fromdist, todist,
-                           frompop, topop, distance,
+                           frompop, topop, popgerm, distance,
                            agegroup, Nages,
                            xcoord, ycoord,density,
                            Ndist, meddist, netactual, ncoefs)
     a ~ filldist(Gamma(5.0, 10.0/4.0),Nages)
     b ~ filldist(Gamma(3.0, 1.0/2.0),Nages)
     c ~ filldist(Gamma(5.0, 1.0/4.0),Nages)
-    d0 ~ filldist(Gamma(5.0, 0.10/4.0),Nages)
+    d0 ~ filldist(Gamma(5.0, 1.0/4.0),Nages)
     neterr ~ Gamma(3.0, 2.0/2.0)
     kd ~ MvNormal(fill(0.0,Nages),0.1*ones(Nages))
+    logisticconst ~ MvNormal(fill(-3.0,Nages),fill(2.0,Nages)) # logistic(-3.0) ~ 0.05 flows are typically on order 5% or less
 
     ## priors for chebychev polys parameters
     desirecoefs ~ MvNormal(zeros(ncoefs*Nages), 1.0 .* ones(ncoefs*Nages))  
@@ -18,14 +19,14 @@
     desfuns = [Fun(Chebyshev(200000.0 .. 600000.0) * Chebyshev(5e6 .. 6.2e6), desirecoefsre[:,i]) for i in 1:Nages]
     desvals = [desfuns[age](xcoord,ycoord) for (xcoord,ycoord) in zip(xcoord,ycoord), age in 1:Nages]
     ## desirability for given age at coordinates as ratio of dest / from
-    desires = [exp((kd[agegroup[i]]*density[todist[i]] +
+    desires = [(kd[agegroup[i]]*density[todist[i]] +
                     desvals[todist[i],agegroup[i]]) - 
                     (kd[agegroup[i]]*density[fromdist[i]] +
-                        desvals[fromdist[i],agegroup[i]]))
+                        desvals[fromdist[i],agegroup[i]])
                for i in 1:length(flows)]
     ## indiviudal flows
-    preds = [frompop[i] * topop[i] * a[agegroup[i]] / 1000.0 *
-        (1.0 + b[agegroup[i]] / (distance[i] / meddist + d0[agegroup[i]])^c[agegroup[i]]) * desires[i]
+    preds = [frompop[i] * logistic(logisticconst[agegroup[i]]  + (topop[i] / popgerm) * a[agegroup[i]] *
+        (1.0 + b[agegroup[i]] / (distance[i] / meddist + d0[agegroup[i]])^c[agegroup[i]]) + desires[i])
              for i in 1:length(flows)]
 
     if typeof(a[1]) != Float64
