81db0d4d55024e6332eea22d5f6f868485b23002
Daniel Lakeland <dlakelan@street-artists.org>
2024-08-09 15:05:30 -0700

    since logistic() is similar to exponential in this range, we convert products to sums

--- model3.jl
+++ model3.jl
@@ -25,7 +25,7 @@
                         desvals[fromdist[i],agegroup[i]])
                for i in 1:length(flows)]
     ## indiviudal flows
-    preds = [frompop[i] * logistic(logisticconst[agegroup[i]]  + (topop[i] / popgerm) * a[agegroup[i]] *
+    preds = [frompop[i] * logistic(logisticconst[agegroup[i]]  + (topop[i] / popgerm) + a[agegroup[i]] +
         (1.0 + b[agegroup[i]] / (distance[i] / meddist + d0[agegroup[i]])^c[agegroup[i]]) + desires[i])
              for i in 1:length(flows)]
 
