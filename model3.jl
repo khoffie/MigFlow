9a4bb988fc1153781b99201803ccdc1feca9742c
Daniel Lakeland <dlakelan@street-artists.org>
2024-08-10 15:56:18 -0700

    Adjust some priors on the coefficient in model3 and then add some constraints on the values of the chebyshev function evaluations, they should all be close to 0 ish

--- model3.jl
+++ model3.jl
@@ -3,12 +3,12 @@
                            agegroup, Nages,
                            xcoord, ycoord,density,
                            Ndist, meddist, netactual, ncoefs)
-    a ~ filldist(Normal(0.0,4.0),Nages)
+    a ~ filldist(Normal(0.0,1.0),Nages)
     b ~ filldist(Gamma(3.0, 1.0/2.0),Nages)
-    c ~ filldist(Gamma(5.0, 1.0/4.0),Nages)
-    d0 ~ filldist(Gamma(5.0, 1.0/4.0),Nages)
+    c ~ filldist(Gamma(5.0, 2.0/4.0),Nages)
+    d0 ~ filldist(Gamma(5.0, 0.2/4.0),Nages)
     neterr ~ Gamma(3.0, 2.0/2.0)
-    logisticconst ~ Normal(-4.0, 2.0) # logistic(-4.0) ~ 0.017 flows are typically on order 5% or less
+    logisticconst ~ Uniform(-12.0,-2.5) # logistic(-4.0) ~ 0.017 flows are typically on order 5% or less
     kd ~ MvNormal(fill(0.0, Nages), (log(5.0) / 0.5) / 2 * ones(Nages)) # density ranges mostly in the range -0.5 to 0.5, so a full-scale change in density could multiply the flow by around 5.0
 
     ## priors for chebychev polys parameters
@@ -18,6 +18,11 @@
     ### array of funs per age group
     desfuns = [Fun(Chebyshev(2e5 .. 1e6) * Chebyshev(5e6 .. 6.2e6), desirecoefsre[:,i]) for i in 1:Nages]
     desvals = [desfuns[age](xcoord,ycoord) for (xcoord,ycoord) in zip(xcoord,ycoord), age in 1:Nages]
+
+    ## as prior for the coefficients, let's tell it that desvals should be near 0 and have standard deviation 1 ish.
+    Turing.@addlogprob!(Normal(0.0,0.25),mean(desvals))
+    Turing.@addlogprob!(Exponential(1.0),std(desvals))
+
     ## desirability for given age at coordinates as ratio of dest / from
     desires = [(kd[agegroup[i]] * density[todist[i]] +
                     desvals[todist[i], agegroup[i]]) -
@@ -29,6 +34,9 @@
         log1p(b[agegroup[i]] / (distance[i] / meddist + d0[agegroup[i]])^c[agegroup[i]]) + desires[i])
              for i in 1:length(flows)]
 
+    if typeof(a[1]) == Float64
+        @printf "a1 = %.2f, b1 = %.2f, c1 = %.2f, d0_1 = %.2f, neterr = %.2f\n"  a[1] b[1] c[1] d0[1] neterr
+    end
     if typeof(a[1]) != Float64
         @printf "a1 = %.2f, b1 = %.2f, c1 = %.2f, d0_1 = %.2f, neterr = %.2f\n"  a[1].value b[1].value c[1].value d0[1].value neterr.value
     end
@@ -48,9 +56,6 @@
 
     ## flows ~ poisson(expectation)
     flows ~ arraydist([Poisson(p) for p in preds])
-    # for c in axes(netactual,2)
-    #     netactual[:,c] ~ MvNormal(netflows[:,c],neterr .* abs.(netflows[:,c]))
-    # end
     
     ### both netactual and flows we optimize together. neterr
     ### determinies the importance of netactual for optimization. the
