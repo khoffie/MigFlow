89efb5d17d3b1386e26edce35b4428881b180af3
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-08-11 12:37:24 +0200

    added missing rows to flows

--- gen_data.R
+++ gen_data.R
@@ -5,28 +5,50 @@ library(sf)
 
 p_clean <- "~/Diss/inst/extdata/clean/"
 flows <- fread(file.path(p_clean, "flows_districts/districts_2000_2017_ger.csv"))
+flows <- flows[year == 2017, .(fromdist = origin, todist = destination,
+                                   year, agegroup = age_group, flows = flow)]
+flows[agegroup == "über65", agegroup := "above65"]
+flows[agegroup == "unter18", agegroup := "below18"]
+flows <- flows[agegroup != "all"]
+
 age_for <- fread(file.path(p_clean, "aux_data", "age17for.csv"))
+setnames(age_for, "age_group", "agegroup")
 age_for[age_group == "all", .(sum(all), sum(german))]
+age_for[agegroup == "über65", agegroup := "above65"]
+age_for[agegroup == "unter18", agegroup := "below18"]
+
 shp <- setDT(sf::read_sf(file.path(p_clean, "/shapes/districts_ext.shp")))
 shp[, year := 2017] ## kind of hacky but otherwise join fails
 density <- data.table::fread(file.path(p_clean, "aux_data", "density.csv"))
 density <- density[, .(region, year, density, bl_ags)]
 
-flows <- flows[year == 2017, .(fromdist = origin, todist = destination,
-                                   year, agegroup = age_group, flows = flow)]
+
+### in flows data all 0 flows are missing. We add them now to make
+### sure all origins have the same destinations for all age groups and
+### vice versa
+all_keys <- CJ(fromdist = regions,
+               todist = regions,
+               agegroup = flows[, unique(agegroup)],
+               year = flows[, unique(year)])
+setkeyv(all_keys, colnames(all_keys))
+setkeyv(flows, colnames(all_keys))
+flows <- flows[all_keys]
+flows[is.na(flows), flows := 0]
+### omitting since all intra-district flows are 0 and this would be
+### hard to explain for the model
+flows <- flows[fromdist != todist]
+
 
 flows <- flows[order(fromdist, todist, year, agegroup)]
-flows[age_for, frompop_ger := i.german / 1e3, on = .(fromdist = region, year, agegroup = age_group)]
-flows[age_for, topop_ger := i.german / 1e3, on = .(todist = region, year, agegroup = age_group)]
-flows[agegroup == "über65", agegroup := "above65"]
-flows[agegroup == "unter18", agegroup := "below18"]
-flows <- flows[agegroup != "all"]
+flows[age_for, frompop := i.german / 1e3, on = .(fromdist = region, year, agegroup)]
+flows[age_for, topop := i.german / 1e3, on = .(todist = region, year, agegroup)]
 
 shp[, pos := sf::st_point_on_surface(geometry)]
 shp[, x := st_coordinates(pos)[, 1]]
 shp[, y := st_coordinates(pos)[, 2]]
 dt_coords <- shp[, .(distcode = AGS, year, name = GEN, xcoord = x / 1e3, ycoord = y / 1e3)]
 dt_coords[density, density := i.density, on = .(distcode = region, year)]
+dt_coords[age_for[agegroup == "all", ], pop := i.german / 1e3, on = .(distcode = region, year)]
 
 ### check if districts are the same
 all(flows[, unique(fromdist)] == flows[order(todist), unique(todist)] )
@@ -47,12 +69,12 @@ distances <- CJ(fromdist = regions, todist = regions)
 distances[dt_coords, c("x_from", "y_from") := .(xcoord, ycoord), on = .(fromdist = distcode)]
 distances[dt_coords, c("x_to", "y_to") := .(xcoord, ycoord), on = .(todist = distcode)]
 distances[, distance := sqrt((x_from - x_to)^2 + (y_from - y_to)^2)]
-flows[distances, distance := as.integer(round(i.distance)), on = .(fromdist, todist)]
+flows[distances, dist := as.integer(round(i.distance)), on = .(fromdist, todist)]
 
-dt_coords
 dt_coords[, .(min = min(xcoord), max = max(xcoord))]
 dt_coords[, .(min = min(ycoord), max = max(ycoord))]
 
+
+
 fwrite(flows, "~/Documents/GermanMigration/data/FlowDataGermans.csv")
 fwrite(dt_coords, "~/Documents/GermanMigration/data/districts.csv")
-
