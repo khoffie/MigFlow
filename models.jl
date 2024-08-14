8d893854a76cbc6077d156e6d3bf31ce7fa49c94
Daniel Lakeland <dlakelan@street-artists.org>
2024-08-14 10:31:04 -0700

    b has been misinterpreted before. It's really the relative size of sensitive/insensitive **for the median distance migration** so since sensitive decays a lot by median distance, it should be a small number, so I'm dividing it by 100 to rescale it.

--- models.jl
+++ models.jl
@@ -118,7 +118,7 @@ etc etc
 
     ## indiviudal flows
     preds = [frompop[i] * logistic(logisticconst + log(topop[i] / popgerm) + a[agegroup[i]] +
-                log1p(b[agegroup[i]] / (distance[i] / meddist + d0[agegroup[i]])^c[agegroup[i]]) + desires[i])
+                log1p(b[agegroup[i]] /100.0 / (distance[i] / meddist + d0[agegroup[i]])^c[agegroup[i]]) + desires[i])
                     for i in 1:length(flows)]
 
     if typeof(a[1]) != Float64
