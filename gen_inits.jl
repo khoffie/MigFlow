7295003847aaa2bc6d49530169da412bbc0d7d94
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-08-15 15:06:30 +0200

    some bound changed

--- gen_inits.jl
+++ gen_inits.jl
@@ -1,35 +1,39 @@
 gen_random_inits = function(Nages, ncoefs)
     inits = [
-        rand(Normal(0.0, 1.0), Nages); #a
-        rand(Uniform(1.5, 2.5), Nages); #c
-        rand(Uniform(0.0, 0.03), Nages); #d0
-        rand(Uniform(0.1, 1), Nages); #dscale
-        [1.5, 8.0]; # neterr and logisticconst
-        fill(0.0, Nages); #kd
-        fill(0.0, Nages * ncoefs) # desirecoefs
+        rand(Normal(0.0, 1.0), Nages); #a prior Exp = 0
+        rand(Uniform(1.5, 2.5), Nages); #c prior Exp = 2.5
+        rand(Uniform(0.3, 2.0), Nages); #d0 prior Exp = 2.5
+        rand(Uniform(0.25, 1), Nages); #dscale prior Exp = 1
+        [1.5];                     # netterr prior Exp = 7.5
+        [8.0];                    # logconst prior Exp = 0
+        fill(0.0, Nages); #kd                prior Exp = 0
+        fill(0.0, Nages * ncoefs) # desirecoefs prior Exp = 0
         ]
   return inits
 end
 
-gen_bounds = function(Nages, ncoefs, cheby_lb, cheby_ub)
+gen_bounds = function(Nages, ncoefs)
     a_lb = - 5.5
     a_ub = 5.5
-
     c_lb = .05
-    c_ub = 10
+    c_ub = 30.0
     d0_lb = 0.0
     d0_ub = 60.0
+    dscale_lb = .02
+    dscale_ub = 3.0
     ne_lb = .5
     ne_ub = 50.0
     lc_lb = -30.0
     lc_ub = 30.0
     kd_lb = -2.5
     kd_ub = 2.5
+    cheby_lb = -10.0
+    cheby_ub = 10.0
         
     lower = [fill(a_lb, Nages); 
             fill(c_lb, Nages); 
             fill(d0_lb, Nages); 
-            fill(0.02,Nages); # dscale in fractions of meddist
+            fill(dscale_lb, Nages); # dscale in fractions of meddist
             [ne_lb, lc_lb];
             fill(kd_lb, Nages); 
             cheby_lb * ones(ncoefs * Nages)]
@@ -37,7 +41,7 @@ gen_bounds = function(Nages, ncoefs, cheby_lb, cheby_ub)
     upper = [fill(a_ub, Nages); 
             fill(c_ub, Nages); 
             fill(d0_ub, Nages); 
-            fill(3.0,Nages); #dscale in fractions of meddist
+            fill(dscale_ub, Nages); #dscale in fractions of meddist
             [ne_ub, lc_ub];
             fill(kd_ub, Nages); 
             cheby_ub * ones(ncoefs * Nages)]
@@ -45,7 +49,7 @@ gen_bounds = function(Nages, ncoefs, cheby_lb, cheby_ub)
 end
 
 
-check_inits = function(Nages, ncoefs) 
+show_inits = function(Nages, ncoefs) 
     names  = [fill("a", Nages); fill("c", Nages); fill("d0", Nages); fill("dscale", Nages); 
               "netterr"; "logconst"; fill("kd", Nages); fill("desire", Nages * ncoefs)]
 
