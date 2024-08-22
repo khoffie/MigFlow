4ed3715df818d54dd136068274483e44e8ddbd1e
Daniel Lakeland <dlakelan@street-artists.org>
2024-08-22 13:52:50 -0700

    code up model for one mm per age group

--- models.jl
+++ models.jl
@@ -95,7 +95,7 @@ https://www.desmos.com/calculator/jhrgbmw9dd
     d0 ~ filldist(Gamma(5.0, 2.0/4.0),Nages)
     dscale ~ filldist(Gamma(8.0,0.5/7.0),Nages) ## sensitive falls to same size as insensitive somewhere around 0.5 times the median distance between districts
     neterr ~ Gamma(3.0, 5/2.0) ## this is in percent
-    mm ~ LocationScale(0.0,1000,Beta(10,100))
+    mm ~ filldist(LocationScale(0.0,1000,Beta(10,100)),Nages)
     kd ~ MvNormal(fill(0.0, Nages), (log(15.0) / 0.5) / 2 * ones(Nages)) # density ranges mostly in the range -0.5 to 0.5, so a full-scale change in density could multiply the flow by around 5.0
 
     ## priors for chebychev polys parameters
@@ -124,9 +124,9 @@ https://www.desmos.com/calculator/jhrgbmw9dd
                 log1p(1.0 / (distance[i] / distscale[agegroup[i]] + d0[agegroup[i]]/100.0)^c[agegroup[i]]) + desires[i])
                     for i in 1:length(flows)]
 
-    if typeof(a[1]) != Float64
-        @printf "a1 = %.2f, c1 = %.2f, d0_1 = %.2f, neterr = %.2f\n"  a[1].value c[1].value d0[1].value neterr.value
-    end
+    #if typeof(a[1]) != Float64
+    #    @printf "a1 = %.2f, c1 = %.2f, d0_1 = %.2f, neterr = %.2f\n"  a[1].value c[1].value d0[1].value neterr.value
+    #end
 
 
     #if typeof(a[1]) != Float64
@@ -143,7 +143,7 @@ https://www.desmos.com/calculator/jhrgbmw9dd
     allmoves ~ Normal(predmoves, .01 * predmoves) ## our total move predictions should be about right by around 1%
 
     ## flows ~ poisson(expectation)
-    flows ~ arraydist([MixtureModel([Poisson(p),Poisson(0.0)],[mm/1000.0,1.0-mm/1000.0]) for p in preds])
+    flows ~ arraydist([MixtureModel([Poisson(p[i]),Poisson(0.0)],[mm[agegroup[i]]/1000.0,1.0-mm[agegroup[i]]/1000.0]) for i in eachindex(preds)])
 
     ### both netactual and flows we optimize together. neterr   
     ### determinies the importance of netactual for optimization. the
