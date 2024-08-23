8e76a4fb84f5db84a7c60fe4a5dc7854e174d555
Konstantin Hoffie <donkon@localhost.localdomain>
2024-08-23 13:53:52 +0200

    migration4 comments and left over code from migration3 deleted

--- models.jl
+++ models.jl
@@ -164,13 +164,11 @@ end
     xcoord, ycoord,density,distpop,
     Ndist, meddist, netactual, ncoefs)
 
-    a ~ filldist(Normal(0.0,2.75),Nages) # center a around a typical value found by plotting the data and adjusting until the overall shift on log scale is about right
-    c ~ filldist(Gamma(10.0, 1.5/9.0),Nages) # c decay rate parameter between about 1 and 4 ish
+    a ~ filldist(Normal(0.0,2.75),Nages)
+    c ~ filldist(Gamma(10.0, 1.5/9.0),Nages) 
     d0 ~ filldist(Gamma(5.0, 2.0/4.0),Nages)
-    dscale ~ filldist(Gamma(8.0,0.5/7.0),Nages) ## sensitive falls to same size as insensitive somewhere around 0.5 times the median distance between districts
-    neterr ~ Gamma(3.0, 5/2.0) ## this is in percent
+    dscale ~ filldist(Gamma(8.0,0.5/7.0),Nages) 
     mm ~ filldist(LocationScale(0.0,1000.0,Beta(2,5)),Nages)
-##     mm ~ LocationScale(0.0,1000.0,Beta(2,5))
 
     distscale = dscale .* meddist
     preds = [frompop[i] * logistic( -3.0 + log(topop[i] / popgerm) + a[agegroup[i]] +
@@ -180,13 +178,8 @@ end
     if any(isnan,preds)
         println("NaN in predictions")
     end
-    ## matrix dist, age
-    netflows = calcnet(preds,fromdist,todist,agegroup,Nages,Ndist)
-
     predmoves = sum(preds) # total predicted flow
     allmoves ~ Normal(predmoves, .01 * predmoves) ## our total move predictions should be about right by around 1%
-
-    ## flows ~ poisson(expectation)
     flows ~ arraydist([MixtureModel([Poisson(preds[i]), Poisson(0.0)],
                                     [mm[agegroup[i]]/1000.0,1.0-mm[agegroup[i]]/1000.0])
                        for i in eachindex(preds)])
