9a52224b6e6423582c61c70f662ffdf30980a5e5
Daniel Lakeland <dlakelan@street-artists.org>
2024-08-30 22:03:05 -0700

    Adjust the model, adding a, ktopop, some other prior info.

--- models.jl
+++ models.jl
@@ -202,34 +202,47 @@ end
 
 
 
-@model function usmodel(flows, allmoves, fromdist, todist, popUS, distance,
+@model function usmodel(flows, allmoves, fromdist, todist, medcpop, distance,
     xcoord, xmin, xmax, ycoord, ymin, ymax, logreldens,densmin, densmax, distpop,
     Ndist, meddist, netactual, ncoefs, ndenscoef)
 
+    a ~ Normal(-14.0,7)
     c ~ Gamma(10.0,1.5/9.0)
     d0 ~ Gamma(5.0,2.0/4.0)
     dscale ~ Gamma(8.0,0.5/7.0)
     neterr ~ Gamma(3.0,5/2.0)
+    ktopop ~ Normal(0.0,5.0)
+    kd ~ MvNormal(zeros(ndenscoef),fill(40.0,ndenscoef))
 
-    kd ~ MvNormal(zeros(ndenscoef),10.0^2 * I(ndenscoef))
+    desirecoefs ~ MvNormal(zeros(ncoefs), fill(50.0,ncoefs))
 
-    desirecoefs ~ MvNormal(zeros(ncoefs), fill(2.0,ncoefs))
-
-    desfun = Fun(Chebyshev(xmin .. xmax) * Chebyshev(ymin .. ymax), desirecoefsre ./ 10) 
+    desfun = Fun(ApproxFun.Chebyshev(xmin .. xmax) * ApproxFun.Chebyshev(ymin .. ymax), desirecoefs ./ 10) 
     desvals = [desfun(xcoord,ycoord) for (xcoord,ycoord) in zip(xcoord,ycoord)]
 
-    densfun = Fun(Chebyshev(densmin .. densmax)*Chebyshev(densmin .. densmax), kd)
+    Turing.@addlogprob!(logpdf(MvNormal(fill(0.0,length(desvals)),fill(3.0,length(desvals))), desvals))
+    ## tamp down the corners of the map
+    Turing.@addlogprob!(logpdf(MvNormal(fill(0.0,4),fill(0.125,4)), [desfun(xmin,ymin), desfun(xmin,ymax),desfun(xmax,ymin), desfun(xmax,ymax)]))
+
+
+    densfun = Fun(ApproxFun.Chebyshev(densmin .. densmax)*ApproxFun.Chebyshev(densmin .. densmax), kd ./ 10)
     distscale = dscale .* meddist
-    preds = [frompop[i] * logistic(densfun(xcoord[fromdist[i],ycoord[fromdist[i]],xcoord[todist[i]],ycoord[todist[i]]] +
-                    log(topop[i]/popUS) + log1p(1.0/(distance[i]/distscale + d0/100.0)^c + desvals[todist[i]]-desval[fromdist[i]]))) for i in eachindex(flows)]
+    preds = [distpop[fromdist[i]] * logistic(densfun(logreldens[fromdist[i]],logreldens[todist[i]]) + a + 
+                    ktopop/10 * log(distpop[todist[i]]/medcpop) + log1p(1.0/(distance[i]/distscale + d0/100.0)^c) + desvals[todist[i]]-desvals[fromdist[i]]) for i in eachindex(flows)]
+    logpreds = log.(preds)
     
+    ## the logarithm of predicted rates is in the range of about -9 to +10 this is more or less a prior on 
+    ## the kd and desfun parameters
+    Turing.@addlogprob!(logpdf(MvNormal(fill(1.0,length(logpreds)),fill(5.0,length(logpreds))),logpreds)) 
+
+    sumpreds = sum(preds)
+    allmoves ~ Normal(sumpreds,0.01*sumpreds)
+
     if any(isnan,preds)
         println("NaN in predictions")
     end
     ## need to create a calcnetUS function for this model, leave it out for the moment
-    predmoves = sum(preds)
-    allmoves ~ Normal(predmoves,.01*predmoves) ## match the overall number of moves to within a close margin
 
     flows ~ arraydist([Poisson(p) for p in preds])
-
+    return (preds=preds)
 end
+
