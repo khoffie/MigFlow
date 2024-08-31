d4b3a11bf39dbfa152a6b224b04e303452f65386
Daniel Lakeland <dlakelan@street-artists.org>
2024-08-30 22:05:13 -0700

    Ensure geog set is SORTED correctly, and that the levelcodes are the same across the datasets. Calculate Haversine distances, and build the model properly.

--- USDataFitting.jl
+++ USDataFitting.jl
@@ -1,5 +1,15 @@
 using CSV,DataFrames,StatsPlots,Distributions,Turing,StatsBase,StatsFuns,FixedWidthTables,DataFramesMeta
 using Printf, CategoricalArrays
+import PlotlyJS
+using CSV, DataFrames, Turing, CategoricalArrays, StatsBase, StatsPlots, Random,
+    ReverseDiff, Revise, RCall, Dates
+using OptimizationOptimJL, Distributions, ApproxFun, Serialization, Printf, DataFramesMeta,
+    StatProfilerHTML, StatsFuns, OptimizationBBO, Printf,OptimizationNLopt,NLopt, LinearAlgebra
+
+using LogDensityProblems,LogDensityProblemsAD, Distances
+
+includet("models.jl")
+
 #using DuckDB
 
 
@@ -118,28 +128,104 @@ end
 
 
 
-function loadallUSdata()
+function loadallUSdata(nzeros = 100000)
 
     flows = loadUS48flows()
     geog = loadUS48geog()
     pop = loadUScountypop()
     pop = DataFramesMeta.@select!(pop,:STATEFP,:COUNTYFP,:POPESTIMATE2016)
     geog = leftjoin(geog,pop,on = [:STATEFP,:COUNTYFP])
-
     meddens = median(geog.POPESTIMATE2016 ./ geog.ALAND)
     geog.logreldens = log.(geog.POPESTIMATE2016 ./ geog.ALAND / meddens)
-    geog
+    geog.countylevel = levelcode.(geog.countyid)
+    sort!(geog,[:countylevel])
+    levels!(flows.fromcounty,levels(geog.countyid))
+    levels!(flows.tocounty,levels(geog.countyid))
 
-    flowset = Set((f.fromcounty,f.tocounty) for f in eachrow(flows))
-    zerosamp = DataFrame((COUNT = 0,fromcounty = f,tocounty = t) for (f,t) in 
-        zip(sample(geog.countyid,10000),sample(geog.countyid,10000)) if !((f,t)  in flowset))
+#    flowset = Set((f.fromcounty,f.tocounty) for f in eachrow(flows))
+#    zerosamp = DataFrame((COUNT = 0,fromcounty = f,tocounty = t) for (f,t) in 
+#        zip(StatsBase.sample(geog.countyid,nzeros),StatsBase.sample(geog.countyid,nzeros)) if !((f,t)  in flowset))
+#    flows = [flows; zerosamp]
 
-    (geog=geog, county = geog, flows = flows, zerosamp=zerosamp)
+    flows.dist = [haversine((geog[levelcode(r.fromcounty),:INTPTLONG],geog[levelcode(r.fromcounty),:INTPTLAT]), 
+                        (geog[levelcode(r.tocounty),:INTPTLONG],geog[levelcode(r.tocounty),:INTPTLAT])) / 1000.0 for r in eachrow(flows)] ## haversine is in m, calculate km
+    mod = usmodel(flows.COUNT,sum(flows.COUNT),levelcode.(flows.fromcounty),
+                levelcode.(flows.tocounty),median(geog.POPESTIMATE2016),flows.dist,
+                geog.INTPTLONG,minimum(geog.INTPTLONG),maximum(geog.INTPTLONG),
+                geog.INTPTLAT,minimum(geog.INTPTLAT),maximum(geog.INTPTLAT),
+                geog.logreldens,minimum(geog.logreldens),maximum(geog.logreldens),
+                geog.POPESTIMATE2016,nrow(geog),100.0,[],36,36)
+    (geog=geog, flows = flows, model=mod) #zerosamp=zerosamp,model=mod)
 end
 
-#=
+if false
+    densdict = Dict(alldata.county.countyid .=> alldata.county.logreldens)
+    histogram2d([densdict[f] for f in alldata.flows.fromcounty],[densdict[f] for f in alldata.flows.tocounty])
 
-densdict = Dict(alldata.county.countyid .=> alldata.county.logreldens)
-histogram2d([densdict[f] for f in alldata.flows.fromcounty],[densdict[f] for f in alldata.flows.tocounty])
 
-=#
+    alldata = loadallUSdata()
+
+    #algo = BBO_de_rand_1_bin_radiuslimited()
+    algo = LBFGS()
+    #algo = NLopt.LN_BOBYQA()
+
+
+    lb = [[-30.0,0.0,0.0,0.05,0.01,-10.0];
+            fill(-20.0,36);
+            fill(-20.0,36)]
+    ub = [[30.0, 5.0,10.0,20.05,4.0,10.0];
+            fill(20.0,36);
+            fill(20.0,36)]
+    ini = rand(Normal(0.0,0.10),length(ub))
+    ini[1:7] .= [-18.0,2.0,1.0,6.0,1.0,3.0,.20] 
+    mapest = maximum_a_posteriori(alldata.model, algo; adtype = AutoReverseDiff(),initial_params=ini,
+        lb=lb,ub=ub, maxiters = 800, maxtime = 600, reltol=1e-3,progress=true)
+    serialize("fitted_models/USmodel_map_$(now()).dat",mapest)
+    println("First 6 coefficients:")
+    println(mapest.values[1:6])
+    #mapest=deserialize("fitted_models/USmodel_map_2024-08-29T17:24:15.073.dat")
+    (densmin,densmax) = (alldata.model.args.densmin, alldata.model.args.densmax)
+    (xmin,xmax) = (alldata.model.args.xmin, alldata.model.args.xmax)
+    (ymin,ymax) = (alldata.model.args.ymin, alldata.model.args.ymax)
+
+    kdindx = 1:36 .+ 6
+    desindx = (1:36) .+ (6+36)
+    kdfun = Fun(ApproxFun.Chebyshev(densmin .. densmax) * ApproxFun.Chebyshev(densmin .. densmax),mapest.values.array[kdindx] ./ 10)
+    desirfun = Fun(ApproxFun.Chebyshev(xmin .. xmax) * ApproxFun.Chebyshev(ymin .. ymax ),mapest.values.array[desindx] ./ 10)
+
+    plotlyjs()# set the background
+
+    p1=heatmap(kdfun;  title="dens Fun (dens,dens)",bins=(40,40),size=(600,600),xlim=(-7,7),ylim=(-7,7)) 
+
+    p2=StatsPlots.histogram2d(StatsBase.sample(alldata.model.args.logreldens,1000),StatsBase.sample(alldata.model.args.logreldens,1000),normalize=true,
+        size=(600,600),xlim=(-7,7),ylim=(-7,7),alpha=.25, title="Distribution of logreldens pairs")
+    StatsPlots.plot(p1,p2; layout=(2,1)) |> display
+
+    PlotlyJS.plot(PlotlyJS.histogram2dcontour(x=StatsBase.sample(alldata.model.args.logreldens,1000), y=StatsBase.sample(alldata.model.args.logreldens,1000)),
+        PlotlyJS.Layout(alpha=0.1, title="Distribution of Randomly chosen Density Pairs",xlab="logreldensity",ylab="logreldensity")) |> display
+
+    heatmap(desirfun, title="Desirability Fun (long,lat)",c=:rainbow) |> display
+
+    preds = generated_quantities(alldata.model,mapest.values.array,names(mapest.values))
+    alldata.flows.preds = preds
+    samps = StatsBase.sample(eachindex(alldata.flows.dist),5000; replace=false)
+
+    flowsamp = alldata.flows[samps,:]
+
+    density(log.(flowsamp.preds); label = "log(preds)", title="Density of log predictions")    
+    density!(log.(flowsamp.COUNT .+ .01); label="actuals + .01") |> display
+    StatsPlots.scatter(flowsamp.dist,log.(flowsamp.COUNT ./ flowsamp.preds); alpha=0.1, title="log(flow/pred) vs dist (in km)") |> display
+
+
+    StatsPlots.scatter(flowsamp.dist,[log.((r.COUNT .+ 1) ./alldata.geog.POPESTIMATE2016[levelcode(r.fromcounty)]) for r in eachrow(flowsamp)];
+        title = "log((flow+1)/from_pop) vs distance", alpha=0.1) |> display
+
+    StatsPlots.scatter(alldata.flows.dist[samps],[log.((preds[s] .+ 1) ./alldata.geog.POPESTIMATE2016[levelcode(alldata.flows.fromcounty[s])]) for s in samps];
+        title = "log((pred+1)/from_pop) vs distance", alpha=0.1) |> display
+
+    kfrom = mapest.values.array[6] / 10.0
+    density(kfrom .* log.(alldata.geog.POPESTIMATE2016[levelcode.(flowsamp.tocounty)] ./ median(alldata.geog.POPESTIMATE2016)); title="DIstribution of log population US * kfrom") |> display
+
+
+    vfit = vi(alldata.model,ADVI(15,200),AutoReverseDiff(true))
+end
