e43cbe3fc40494a592723ad3b457dbb1fe4eecd9
Konstantin Hoffie <donkon@localhost.localdomain>
2024-10-16 11:28:38 +0200

    cleanup

--- densitymodel.jl
+++ densitymodel.jl
@@ -3,11 +3,7 @@ Pkg.activate(".")
 using Turing, CSV, DataFrames, StatsPlots, Plots
 
 df = CSV.read("./manuscript_input/germdensfun_18-25.csv", DataFrame)
-districts = CSV.read("./data/districts.csv", DataFrame)
-meddens = median(districts.density)
-df.fromdens = exp.(df.fromdens) .* meddens    
-df.todens = exp.(df.todens) .* meddens    
-
+# districts = CSV.read("./data/districts.csv", DataFrame)
 histogram(df.funval)
 scatter(df.fromdens, df.funval)
 scatter(df.todens, df.funval)
@@ -22,16 +18,6 @@ scatter(df.todens, df.funval)
     return preds
 end
 
-@model function densmod(fd, td, y)
-    a ~ Normal(1, 5)
-    b ~ Normal(0, 5)
-    s ~ truncated(Normal(3, 3), 0, Inf)
-
-    preds = [ (td[i] / fd[i])^(a + b * fd[i]) for i in 1:length(fd)]
-    y ~ arraydist([Normal(m, s) for m in preds])
-    return preds
-end
-
 model = densmod(df.fromdens, df.todens, df.funval)                       
 chain = Turing.sample(model, NUTS(), 1000)
 plot(chain)
@@ -40,6 +26,6 @@ df.preds = generated_quantities(model, chain[1000])[1]
 cc = round(cor(df.preds, df.funval), digits = 2)
 plot(df.preds, df.funval, seriestype = :scatter,
      xlabel = "Prediction", ylabel = "Cheby value",
-     title = "Densiy model", label = "Correlation = $(cc)")
+     title = "Density model", label = "Correlation = $(cc)")
 
 
