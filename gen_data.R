53994ecb498ab0afc98a524fdb5eca346fde2d02
Konstantin Hoffie <donkon@localhost.localdomain>
2024-10-21 10:46:22 +0200

    fixed bugs

--- gen_data.R
+++ gen_data.R
@@ -1,15 +1,14 @@
 library(data.table)
-library(MigStat)
+library(helpeR)
 library(sfheaders)
 library(sf)
 
-source("functions.R")
 
 p_clean <- "~/Diss/inst/extdata/clean/"
 flows <- fread(file.path(p_clean, "flows_districts/districts_2000_2017_ger.csv"))
 age_for <- fread(file.path(p_clean, "aux_data", "age17for.csv"))
 setnames(age_for, "age_group", "agegroup")
-rec_ages(age_for)
+helpeR::rec_ages(age_for)
 shp <- setDT(sf::read_sf(file.path(p_clean, "/shapes/districts_ext.shp")))
 density <- data.table::fread(file.path(p_clean, "aux_data", "density.csv"))[
                          , .(region, year, density, bl_ags)]
@@ -19,7 +18,7 @@ clean_flows <- function(flows, age_dt) {
                .(fromdist = origin, todist = destination, year,
                  agegroup = age_group, flows = flow)]
     rec_ages(flows)
-    flows <- add_mising_flows(flows, flows[, unique(fromdist)],
+    flows <- helpeR::add_missing_flows(flows, flows[, unique(fromdist)],
                               flows[, unique(agegroup)], flows[, unique(year)])
     ### omitting since all intra-district flows are 0 and this would be
     ### hard to explain for the model
@@ -42,24 +41,24 @@ gen_coords_dt <- function(shp, age_dt, density_dt) {
 
     dt[density, density := i.density, on = .(distcode = region, year)]
     dt[age_for[agegroup == "all", ], pop := i.german, on = .(distcode = region, year)]
-    dt_coords <- dt_coords[, .(distcode, year, pop, density, name, xcoord, ycoord, bl_ags, bl_name)]
+    dt <- dt[, .(distcode, year, pop, density, name, xcoord, ycoord, bl_ags, bl_name)]
     return(dt)
 }
 
-check_tables <- function(flows, coords_dt) {
+check_tables <- function(flows, coords) {
     stopifnot(all(flows[, unique(fromdist)] == flows[order(todist), unique(todist)] ))
-    stopifnot(all(flows[, unique(fromdist)] == dt_coords[, distcode]))
-    dt_coords[distcode %in% c(5315, 2000, 11000, 14713, 9162, 1001),
+    stopifnot(all(flows[, unique(fromdist)] == coords[, distcode]))
+    coords[distcode %in% c(5315, 2000, 11000, 14713, 9162, 1001),
               .(name, xcoord = xcoord, ycoord = ycoord)]
-    dt_coords[, .(min = min(xcoord), max = max(xcoord))]
-    dt_coords[, .(min = min(ycoord), max = max(ycoord))]
+    coords[, .(min = min(xcoord), max = max(xcoord))]
+    coords[, .(min = min(ycoord), max = max(ycoord))]
 }
 
-calculate_distances <- function(flows, coords_dt) {
-    regions  <- dt_coords[, unique(distcode)]
+calculate_distances <- function(flows, coords) {
+    regions  <- coords[, unique(distcode)]
     distances <- CJ(fromdist = regions, todist = regions)
-    distances[dt_coords, c("x_from", "y_from") := .(xcoord, ycoord), on = .(fromdist = distcode)]
-    distances[dt_coords, c("x_to", "y_to") := .(xcoord, ycoord), on = .(todist = distcode)]
+    distances[coords, c("x_from", "y_from") := .(xcoord, ycoord), on = .(fromdist = distcode)]
+    distances[coords, c("x_to", "y_to") := .(xcoord, ycoord), on = .(todist = distcode)]
     distances[, distance := sqrt((x_from - x_to)^2 + (y_from - y_to)^2)]
     flows[distances, dist := as.integer(round(i.distance)), on = .(fromdist, todist)]
     return(NULL)
