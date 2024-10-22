879adab31137c271ad15cc6942639abf59dc2c50
Konstantin Hoffie <donkon@localhost.localdomain>
2024-10-22 08:47:35 +0200

    savelp()

--- teststuff.jl
+++ teststuff.jl
@@ -1,14 +1,22 @@
-using StatsBase
+using Pkg
+Pkg.activate(".")
+using StatsBase, Serialization, Turing, Plots
 germ = loadallGermData(; sample = false)
 us = loadallUSdata()
 
+savelp = function()
+    path = "./manuscript_input"
+    out = Dict{String, Any}()  # Create a dictionary to store the plots
+    ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
+    for age in ages
+        chain = deserialize(path * "/germchain_$(age)")
+        out[age] = Plots.plot(chain[:lp], title = "LP for $(age)")
+    end
+    p = Plots.plot(out["below18"], out["18-25"], out["25-30"],
+                   out["30-50"], out["50-65"], out["above65"],
+                   layout = (3, 2))
+    savefig(path * "/plots/lp_values.pdf")
+end
 
-sample = true    
-test = loadallGermData(0; sample = sample)
-test = loadallUSdata(0; sample = sample)
-nzeros = 0
-names(geog)
 
-@which maximum_a_posteriori
-
-?Turing.Optimisation.maximum_a_posteriori
+savelp()
