1904968b8c91597cba902cb385bac529445eaf02
Konstantin Hoffie <donkon@localhost.localdomain>
2024-10-23 10:48:23 +0200

    cleanup

--- analyze_julia_out.jl
+++ analyze_julia_out.jl
@@ -2,23 +2,23 @@ using Pkg
 Pkg.activate(".")
 using Turing, CSV, DataFrames, StatsPlots, Plots, LaTeXStrings, Serialization
 
-plot_surface = function(df, age, max)
-    Plots.surface(df.todens, df.fromdens, df.funval,
-                  title = "Cheby density, agegroup $(age)\n
+plot_surface = function()
+    plot_surface_ = function(df, age, max)
+        Plots.surface(df.todens, df.fromdens, df.funval,
+                      title = "Cheby density, agegroup $(age)\n
                   max logreldens = $(max)",
-                  xlab = L"$\rho_o$", ylab = L"$\rho_d$",
-                  camera = (10, 45))
+                      xlab = L"$\rho_o$", ylab = L"$\rho_d$",
+                      camera = (10, 45))
+    end
+    ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
+    for age in ages
+        df = CSV.read("./manuscript_input/germdensfun_$(age).csv", DataFrame)
+        df = df[df.fromdens .< 2.7, :]
+        df = df[df.todens .< 2.7, :]
+        p = plot_surface_(df, age, 2.7)
+        savefig("./manuscript_input/plots/dens_$(age).pdf")
+    end
 end
-
-ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
-for age in ages
-    df = CSV.read("./manuscript_input/germdensfun_$(age).csv", DataFrame)
-    df = df[df.fromdens .< 2.7, :]
-    df = df[df.todens .< 2.7, :]
-    p = plot_surface(df, age, 2.7)
-    savefig("./manuscript_input/plots/dens_$(age).pdf")
-end
-
 
 savelp = function()
     path = "./manuscript_input"
@@ -35,3 +35,4 @@ savelp = function()
 end
 
 savelp()
+plot_surface()
