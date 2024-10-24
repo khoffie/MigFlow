1d5b0114f6a5d57691a006f6edd28eb15424a813
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-10-24 17:42:29 +0200

    checking pars

--- teststuff.jl
+++ teststuff.jl
@@ -3,3 +3,22 @@ Pkg.activate(".")
 using StatsBase, Serialization, Turing, Plots
 germ = loadallGermData(; sample = false)
 us = loadallUSdata()
+
+
+parnames = [["a", "c", "d0", "dscale", "ktopop"];
+            ["kd[$i]" for i in 1:36];
+            ["desirecoefs[$i]" for i in 1:36]]
+
+lb = [[-60.0, 0.0, 0.0, 1.0, -10.0];
+      fill(-50.50, 36);
+      fill(-50.50, 36)]
+
+ub = [[60.0, 20.0, 10.0, 15.0, 10.0];
+      fill(50.50, 36);
+      fill(50.50, 36)]
+
+ini = rand(Normal(0.0, 0.10), length(ub))
+ini[1:5] .= [-7.6, 1.81, 1.5, 5.0, 3.5]
+
+new = DataFrame([ :pars => parnames, :lb => lb, :inits => ini, :ub => ub])
+
