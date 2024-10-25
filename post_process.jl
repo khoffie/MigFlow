38e9608b06025821586aaaa6865571dc00fb6653
Konstantin Hoffie <donkon@localhost.localdomain>
2024-10-25 13:53:08 +0200

    showing last lp in lp plots

--- post_process.jl
+++ post_process.jl
@@ -21,19 +21,22 @@ plot_surface = function()
 end
 
 savelp = function()
-    path = "./manuscript_input"
+    path = "./manuscript_input/bu"
     out = Dict{String, Any}()  # Create a dictionary to store the plots
     ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
     for age in ages
         chain = deserialize(path * "/germchain_$(age)")
-        out[age] = Plots.plot(chain[:lp], title = "LP for $(age)")
+        lastlp = chain[:lp][length(chain)]
+        lastlp = round(lastlp, digits = 0)
+        out[age] = Plots.plot(chain[:lp],
+                              title = "LP for $(age)",
+                              label = "lastlp: $(lastlp)")
     end
     p = Plots.plot(out["below18"], out["18-25"], out["25-30"],
                    out["30-50"], out["50-65"], out["above65"],
                    layout = (3, 2))
-    savefig(path * "/plots/lp_values.pdf")
+    savefig(path * "/plots/lp_values_old.pdf")
 end
-
 
 plot_distance = function()
     path = "./manuscript_input"
