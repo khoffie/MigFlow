43043c52339800a64f27eeea5ff143bb24bd3ccb
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-11-03 17:45:08 +0100

    little simulation exercise

--- teststuff.jl
+++ teststuff.jl
@@ -1,5 +1,27 @@
-using Pkg
+using Turing: @addlogprob!
+lusing Pkg
 Pkg.activate(".")
 using StatsBase, Serialization, Turing, Plots
-germ = loadallGermData(; sample = false)
-us = loadallUSdata()
+
+
+function fit(;add = true)
+    @model function foo()
+        X ~ Normal(200, 1)
+        Y ~ Normal(200, 1)
+        if add 
+            Turing.@addlogprob!(logpdf(Normal(1, .25), sqrt(X^2 + Y^2)))
+        end
+    end
+    chain = Turing.sample(foo(), NUTS(), 1000)
+    lp = round(chain[:lp][end]; digits = 2)
+    p = plot(chain[:X], chain[:Y], seriestype = :scatter,
+             label = "LP = $(lp)")
+    display(p)
+    return chain
+end
+
+chain = fit(add = true)
+
+
+
+
