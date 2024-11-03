271e06b212783e01d1ea3f842d2764c308d93c0f
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-11-03 21:43:25 +0100

    more plots

--- teststuff.jl
+++ teststuff.jl
@@ -1,27 +1,51 @@
 using Turing: @addlogprob!
-lusing Pkg
+using Pkg
 Pkg.activate(".")
-using StatsBase, Serialization, Turing, Plots
+using StatsBase, Serialization, Turing, Plots, StatsPlots, LaTeXStrings
 
+function marginal(x)
+    m = round(mean(x); digits = 2)
+    sd = round(std(x); digits = 2)
+    p = density(x, label = L"\mu = %$m, \sigma = %$sd")
+    display(p)
+    return p
+end
+
+function joint()
+    lp = round(chain[:lp][end]; digits = 2)
+    p = plot(chain[:X], chain[:Y], seriestype = :scatter,
+             label = "LP = $(lp)")
+    return p
+end
 
 function fit(;add = true)
     @model function foo()
-        X ~ Normal(200, 1)
-        Y ~ Normal(200, 1)
+        X ~ Normal(200, 10)
+        Y ~ Normal(20, 10)
         if add 
             Turing.@addlogprob!(logpdf(Normal(1, .25), sqrt(X^2 + Y^2)))
         end
     end
     chain = Turing.sample(foo(), NUTS(), 1000)
-    lp = round(chain[:lp][end]; digits = 2)
-    p = plot(chain[:X], chain[:Y], seriestype = :scatter,
-             label = "LP = $(lp)")
-    display(p)
-    return chain
+    x = marginal(chain[:X])
+    y = marginal(chain[:Y])
+    j = joint()
+    return j, x, y
 end
 
-chain = fit(add = true)
+function fitall()
+    j, x, y = fit(; add = false)
+    j2, x2, y2 = fit(; add = true)
+    plt = plot(x, y, x2, y2, p2)
+    display(plt)
+    return p
+end
 
 
+j, x, y = fit(; add = false)
+j2, x2, y2 = fit()
+
+plot(j, j2)
+plot(x, y, x2, y2, p2, layout = (3, 2))
 
 
