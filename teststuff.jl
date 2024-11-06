9e05a250ffe3b06240abe2e2e13d33b80200c9d9
Konstantin Hoffie <donkon@localhost.localdomain>
2024-11-06 14:27:32 +0100

    cleanup

--- teststuff.jl
+++ teststuff.jl
@@ -2,38 +2,9 @@ using Pkg
 Pkg.activate(".")
 using StatsBase, Serialization, Turing, Plots, StatsPlots, LaTeXStrings, Revise
 
-Revise.includet("./writeup/prior/jointprior.jl")
+path = "manuscript_input/2024-11-06_13-37-41"
+chain = deserialize(joinpath(path, "germchain_below18.csv"))
 
-##        d = -.5(((x - μ₁) / σ₁)² + ((y - μ₂)/ σ₂)² + ((√(x² + y²) - μ₃) / σ₃)²)
-        # z = ((√(x^2 + y^2) - μ₃) / σ₃)^2
-        # d = -.5(((x - μ₁) / σ₁)^2 + ((y - μ₂)/ σ₂)^2 + ((z - μ₃) / σ₃ * √2)^2)
+plot(chain[:c])
+plot(chain[:lp])
 
-function fitmodel(add)
-    chain = Turing.sample(custom_model(add), NUTS(), 1000)
-    xy = plot(chain[:x], chain[:y], seriestype = :scatter, label = L"(X, Y)")
-    x = density(chain[:x], label = L"X")
-    y = density(chain[:y], label = L"Y")
-    xyd = density(sqrt.(chain[:x].^2 .+ chain[:y].^2), label = L"\sqrt{(X^2 + Y^2)}")
-    p = plot(x, y, xy, xyd)
-    display(p)
-    return chain, p
-end
-
-@model function custom_model(add = true)
-    μ₁ = 0
-    μ₂ = 0
-    μ₃ = 10
-    σ₁ = 10
-    σ₂ = 10
-    σ₃ = 0.1
-    x ~ Normal(μ₁, σ₁)
-    y ~ Normal(μ₂, σ₂)
-##    z ~ Normal(μ₃, σ₃)
-    function density(x, y)
-        d = -.5(((x - μ₁) / σ₁)^2 + ((y - μ₂)/ σ₂)^2 + ((√(x^2 + y^2) - μ₃) / σ₃)^2)
-        return d
-    end
-    if add; Turing.@addlogprob!(density(x, y)); end
-end
-
-fitmodel(true)
