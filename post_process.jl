ec24465bdaa9fd3513e49dee55386f35b3ecf802
Konstantin Hoffie <donkon@localhost.localdomain>
2024-11-08 11:51:53 +0100

    post_process() plots julia plots for all years

--- post_process.jl
+++ post_process.jl
@@ -7,12 +7,15 @@ plot_surface = function(path)
                       camera = (45, 45))
     end
     ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
-    for age in ages
-        df = CSV.read(joinpath(path,  "germdensfun_$(age).csv"), DataFrame)
-        df = df[df.fromdens .< 2.7, :]
-        df = df[df.todens .< 2.7, :]
-        p = plot_surface_(df, age, 2.7)
-        savefig(joinpath(path, "plots/dens_$(age).pdf"))
+    years = 2011 : 2017
+    for y in years
+        for a in ages
+            df = CSV.read(joinpath(path,  "germdensfun_$(y)_$(a).csv"), DataFrame)
+            df = df[df.fromdens .< 2.7, :]
+            df = df[df.todens .< 2.7, :]
+            p = plot_surface_(df, a, 2.7)
+            savefig(joinpath(path, "plots/dens_$(y)_$(a).pdf"))
+        end
     end
 end
 
@@ -30,15 +33,18 @@ savelp = function(path, from = nothing, to = nothing)
     end
     out = Dict{String, Any}()  # Create a dictionary to store the plots
     ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
-    for age in ages
-        chain = deserialize(joinpath(path ,"germchain_$(age).csv"))
-        out[age] = plotlp(chain, age, from, to)
-    end
-    p = Plots.plot(out["below18"], out["18-25"], out["25-30"],
-                   out["30-50"], out["50-65"], out["above65"],
-                   layout = (3, 2))
+    years = 2011 : 2017
+    for y in years
+        for a in ages
+            chain = deserialize(joinpath(path ,"germchain_$(y)_$(a).csv"))
+            out[a] = plotlp(chain, a, from, to)
+        end
+        p = Plots.plot(out["below18"], out["18-25"], out["25-30"],
+                       out["30-50"], out["50-65"], out["above65"],
+                       layout = (3, 2))
     display(p)
-    savefig(joinpath(path, "plots/lp_values.pdf"))
+    savefig(joinpath(path, "plots/lp_values_$(y).pdf"))
+    end
 end
 
 plot_distance = function(path)
@@ -49,23 +55,26 @@ plot_distance = function(path)
     end
     out = Dict{String, Any}()  # Create a dictionary to store the plots
     ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
-    for age in ages
-        df = CSV.read(joinpath(path, "germflows_$(age).csv"), DataFrame)
-        N = nrow(df)
-        size = 2000
-        df = df[Random.shuffle(1:N)[1:min(size, N)], :]
-        df.out = log.((df.flows .+ 1) ./ df.preds)
-        out[age] = plot_distance_(df, age)
+    years = 2011 : 2017
+    for y in years
+        for a in ages
+            df = CSV.read(joinpath(path, "germflows_$(y)_$(a).csv"), DataFrame)
+            N = nrow(df)
+            size = 2000
+            df = df[Random.shuffle(1:N)[1:min(size, N)], :]
+            df.out = log.((df.flows .+ 1) ./ df.preds)
+            out[a] = plot_distance_(df, a)
+        end
+        p = Plots.plot(out["below18"], out["18-25"], out["25-30"],
+                       out["30-50"], out["50-65"], out["above65"],
+                       layout = (3, 2))
+        display(p)
+        savefig(joinpath(path,  "plots/distance_$(y).pdf"))
     end
-    p = Plots.plot(out["below18"], out["18-25"], out["25-30"],
-                   out["30-50"], out["50-65"], out["above65"],
-                   layout = (3, 2))
-    display(p)
-    savefig(joinpath(path,  "plots/distance.pdf"))
 end
 
 post_process = function(path = nothing, lp_from = nothing, lp_to = nothing, render_doc = true)
-    isnothing(path) ? path = path = readline("./writeup/juliaout_path.txt")[1] : path
+    isnothing(path) ? path = path = readline("./writeup/juliaout_path.txt") : path
     println(path)
     mkpath(joinpath(path, "plots"))
     savelp(path, lp_from, lp_to)
@@ -78,4 +87,3 @@ post_process = function(path = nothing, lp_from = nothing, lp_to = nothing, rend
     if isfile(f);  rm(f); end
     render_doc ? R"helpeR::render_doc('./writeup', 'report.Rmd')" : println("Report not generated")
 end
-
