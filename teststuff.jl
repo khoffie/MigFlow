36d5ac5593755620e037097bbf83440e54204187
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-11-21 12:37:47 +0100

    tried kde method

--- teststuff.jl
+++ teststuff.jl
@@ -1,29 +1,31 @@
-includet("src/datafitting.jl")
+using Revise
+includet("main.jl")
+using KernelDensity
 
-path = "./manuscript_input/tempered"
+path = "./manuscript_input/30kMH"
 f = "germchain_2017_30-50.csv"
 
 chain = deserialize(joinpath(path, f))
-geodat, agedat = loadallGermData(sample = false, positive_only = true)
-agedat = filter(:agegroup => ==("30-50"), agedat)
+districts, flows = loadallGermData(sample = false, positive_only = true)
+flows = filter(:agegroup => ==("30-50"), flows)
 
-logreldens = log.(geodat.density / median(geodat.density))
-minlrd = minimum(logreldens)
-maxlrd = maximum(logreldens)
+p = densitychains(chain, flows, districts, 1000)
+p
 
-function densheatmap(chain, densmin, densmax, logreldens, title, type = nothing )
-    pars = OrderedDict(zip(string.(chain.value.axes[2]), chain.value.data[end, :, 1]))
-    kds = [k for k in keys(pars) if contains(k, "kd")]
-    kdfun = Fun(ApproxFun.Chebyshev(densmin .. densmax) * ApproxFun.Chebyshev(densmin .. densmax),
-                getindex.(Ref(pars), kds) ./ 10)
-    p = Plots.heatmap(kdfun, title = type)
-    return p
-end
-
-p1 = densheatmap(chain, minlrd, maxlrd, "logreldens")
-p2 = densheatmap(chain, 0, 5000, "untransformed", "value")
-plot(p1, p2)
-p1
+lrd = sort(log.(districts.density / median(districts.density)))
+minlrd = minimum(lrd)
+maxlrd = maximum(lrd)
 
 
-p = densitychains(chain, flows, districts, 100)
+pars = OrderedDict(zip(string.(chain.value.axes[2]), chain.value.data[end, :, 1]))
+kds = [k for k in keys(pars) if contains(k, "kd")]
+kdfun = Fun(ApproxFun.Chebyshev(minlrd .. maxlrd) * ApproxFun.Chebyshev(minlrd .. maxlrd),
+            getindex.(Ref(pars), kds) ./ 10)
+xes = StatsBase.sample(lrd, 100)
+yes = StatsBase.sample(lrd, 100)
+ikd = InterpKDE(kde((xes, yes)))
+interval = minlrd : .1 : maxlrd
+p = Plots.heatmap([kdfun * (pdf(ikd, x, y) > 0.1) for x in interval, y in interval])
+p = Plots.heatmap([1 * (pdf(ikd, x, y) > 0.1) for x in interval, y in interval])
+
+param(chain, :lp, 50)
