e2b924a0f7be500cada501ab47de30af7be0df44
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-11-21 17:19:34 +0100

    using kde method successfully now

--- src/densheatmap.jl
+++ src/densheatmap.jl
@@ -1,20 +1,21 @@
-function densitychains(chain::Chains, flows::DataFrame, districts::DataFrame, n::Int, title = nothing)
-##    fromdens, todens, mi, ma = densodensd(flows, districts, n)
-    plts = [densheatmap(chain[:, :, i])[1] for i in 1 : size(chain)[3]]
+function densitychains(chain::Chains, flows::DataFrame, districts::DataFrame, n::Int,
+                       dens_th, title = nothing)
+    fromdens, todens, densmin, densmax = densodensd(flows, districts, n)
+    plts = [densheatmap(chain[:, :, i], fromdens, todens, densmin, densmax, dens_th)[1]
+            for i in 1 : size(chain)[3]]
     p = Plots.plot(plts..., plot_title = title == nothing ? "" : title)
     return p
 end
 
-function densheatmap(chain)
-    densmin = -1.713679028413716
-    densmax = 3.1516041062884512
+function densheatmap(chain, fromdens, todens, densmin, densmax, dens_th)
     pars = OrderedDict(zip(string.(chain.value.axes[2]), chain.value.data[end, :, 1]))
     kds = [k for k in keys(pars) if contains(k, "kd")]
     kdfun = Fun(ApproxFun.Chebyshev(densmin .. densmax) * ApproxFun.Chebyshev(densmin .. densmax),
                 getindex.(Ref(pars), kds) ./ 10)
-    # values = [kdfun(x, y) for x in denso, y in densd]
-    # p = Plots.heatmap(denso, densd, values, colorbar = false, ticks = false)
-    p = Plots.heatmap(kdfun, colorbar = false)
+    ikd = InterpKDE(kde((fromdens, todens)))
+    interval = densmin : .1 : densmax
+    p = heatmap(interval, interval, [kdfun(x, y) * (pdf(ikd, x, y) > dens_th)
+                                     for y in interval, x in interval])
     return p, values
 end
 
@@ -28,9 +29,8 @@ function densodensd(flows, districts, n)
     leftjoin!(df, densities, on = :todist => :distcode)
     rename!(df, :density => "todens")
 
-    df1 = df[shuffle(1 : nrow(df))[1 : n], [:fromdens, :todens]]
-    fromdens = sort(df1.fromdens)
-    todens = sort(df1.todens)
-    return fromdens, todens, minlrd, maxlrd
+    df1 = df[shuffle(1 : nrow(df))[1 : n], :]
+    df1.todens = collect(skipmissing(df1.todens)) ## Vector{Union{Missing, Float64}}
+    return df1.fromdens, df1.todens, minlrd, maxlrd
 end
 
--- teststuff.jl
+++ teststuff.jl
@@ -2,30 +2,20 @@ using Revise
 includet("main.jl")
 using KernelDensity
 
-path = "./manuscript_input/30kMH"
+path = "./manuscript_input/tempered"
 f = "germchain_2017_30-50.csv"
 
 chain = deserialize(joinpath(path, f))
 districts, flows = loadallGermData(sample = false, positive_only = true)
 flows = filter(:agegroup => ==("30-50"), flows)
 
-p = densitychains(chain, flows, districts, 1000)
+p = densitychains(chain, flows, districts, 1000, 0.01)
 p
 
-lrd = sort(log.(districts.density / median(districts.density)))
-minlrd = minimum(lrd)
-maxlrd = maximum(lrd)
+fromdens, todens, densmin, densmax = densodensd(flows, districts, 1000)
+p, v = densheatmap(chain, fromdens, todens, densmin, densmax)
 
 
-pars = OrderedDict(zip(string.(chain.value.axes[2]), chain.value.data[end, :, 1]))
-kds = [k for k in keys(pars) if contains(k, "kd")]
-kdfun = Fun(ApproxFun.Chebyshev(minlrd .. maxlrd) * ApproxFun.Chebyshev(minlrd .. maxlrd),
-            getindex.(Ref(pars), kds) ./ 10)
-xes = StatsBase.sample(lrd, 100)
-yes = StatsBase.sample(lrd, 100)
-ikd = InterpKDE(kde((xes, yes)))
-interval = minlrd : .1 : maxlrd
-p = Plots.heatmap([kdfun * (pdf(ikd, x, y) > 0.1) for x in interval, y in interval])
-p = Plots.heatmap([1 * (pdf(ikd, x, y) > 0.1) for x in interval, y in interval])
+@which densheatmap(chain, fromdens, todens, densmin, densmax, dens_th)
 
-param(chain, :lp, 50)
+p, v = densheatmap(chain, fromdens, todens, densmin, densmax)
