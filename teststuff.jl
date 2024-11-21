6ad83520cc8278c9737d91677d9a6c7898856c26
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-11-21 18:02:59 +0100

    cleanup

--- teststuff.jl
+++ teststuff.jl
@@ -10,19 +10,9 @@ districts, flows = loadallGermData(sample = false, positive_only = true)
 flows = filter(:agegroup => ==("30-50"), flows)
 
 p = densitychains(chain, flows, districts, 1000, 0.01, "Density preferences, 30-50, 2017")
-p
+
 
 fromdens, todens, densmin, densmax = densodensd(flows, districts, 10000)
 p, v = densheatmap(chain, fromdens, todens, densmin, densmax)
 
-
-@which densheatmap(chain, fromdens, todens, densmin, densmax, dens_th)
-
-p, v = densheatmap(chain, fromdens, todens, densmin, densmax, 0.01)
-
-function zerotonan(x)
-    y = x == 0.0 ? NaN : x
-    return y
-end
-zerotonan.(v)
 
