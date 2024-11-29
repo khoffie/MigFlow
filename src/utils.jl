9f6ef0b84d81e71ea54dcbb7c98b2c6736665001
Konstantin Hoffie <donkon@localhost.localdomain>
2024-11-29 12:38:38 +0100

    chainnames()

--- src/utils.jl
+++ src/utils.jl
@@ -5,3 +5,15 @@ function decaytime(y₀, yₜ, r, steptime = 5)
     out = (steps = round(s, digits = 2), time = round(t, digits = 2))
     return out
 end
+
+function chainnames(path, temp = nothing)
+    fchains = [f for f in readdir(path) if contains(f, "germchain")]
+    if temp == nothing
+        fchains = fchains[.!contains.(fchains, ".0.csv")]
+    elseif temp == "all"
+        fchains
+    else
+        fchains = fchains[contains.(fchains, "$(temp).0.csv")]
+    end
+    return fchains
+end
