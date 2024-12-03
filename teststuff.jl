35ec9b915388503318dc6f20c07cb571e0a19f2f
Konstantin Hoffie <donkon@localhost.localdomain>
2024-12-03 09:32:03 +0100

    cleanup

--- teststuff.jl
+++ teststuff.jl
@@ -1,8 +1,7 @@
 using Revise
 includet("main.jl")
 
-path = "./manuscript_input/pretempering/final"
-
+path = "./manuscript_input/30kMH"
 jo = "./writeup/juliaout_path.txt"
 write(jo, path)
 
@@ -14,17 +13,6 @@ postprocess(path = path, render_doc = false, denstype = "best")
 chain = deserialize(joinpath(path, f))
 flows, districts = loadallGermData(sample = false, positive_only = true)
 
-
-
-fromdens, todens, densmin, densmax = densodensd(flows, districts, 10000)
-p, v = densheatmap(chain, fromdens, todens, densmin, densmax)
-postprocess()
-
-r = 0.1
-λ = log(1 - r)
-yₜ = 10
-y₀ = 10000
-
-
-decaytime(10000, 1, .1, 10)
+path
+postprocess(path = path, render_plots = true, denstype = "best")
 
