fdad0d753d0b4caa0ff08544676e0712b5eb9caa
Konstantin Hoffie <donkon@localhost.localdomain>
2024-12-16 14:11:55 +0100

    nothing

--- teststuff.jl
+++ teststuff.jl
@@ -5,3 +5,10 @@ path = "./manuscript_input/144temp"
 fchains = chainnames(path, "144")
 chain = deserialize(joinpath(path, fchains[1]))
 plot(chain[:lp])
+
+chains = chainnames("/home/donkon/koho/sampling")
+
+f = "./writeup/juliaout_path.txt"
+write(f, "./manuscript_input/30kMH")
+
+postprocess(render_plots = false)
