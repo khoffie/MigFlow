bcc393abb5f66cded3e662a080c56c71ef87da9b
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-01-25 23:13:57 +0100

    creating clean data dirs, typo fixed

--- raw_to_clean.R
+++ raw_to_clean.R
@@ -9,19 +9,23 @@ library(readxl)
 
 
 rawdata_to_cleandata <- function(raw, clean) {
-    helpeR::preprocess_germanflows(file.path(raw, "german_flows"),
-                                   file.path(clean, "german_flows"))
-    ## This may create an NA.csv which makes german_flows() fail.
-    helpeR::german_flows(file.path(clean, "german_flows"),
-                         file.path(clean, "flows_districts_2000_2017_ger.csv"))
+  dir.create(file.path(clean))
+  dir.create(file.path(clean, "german_flows"))
+  helpeR::preprocess_germanflows(file.path(raw, "german_flows"),
+                                 file.path(clean, "german_flows"))
+  ## This may create an NA.csv which makes german_flows() fail.
+  helpeR::german_flows(file.path(clean, "german_flows"),
+                       file.path(clean, "flows_districts_2000_2017_ger.csv"))
 
-    helpeR::german_popdata(raw, clean)
+  helpeR::german_popdata(raw, clean)
 
-    ## German auxilliary data
-    german_administrative_changes(file.path(raw, "ref-kreise-umrech-2019-1990-2018.xlsx"),
-                                  file.path(clean, "correct.csv"))
-    file.copy(file.path(raw, "density.csv"), file.path(clean, "density.csv"),
-              overwrite = TRUE)
-    file.copy(file.path(raw, "shapes"), file.path(clean, "shapes"),
-              recursive = TRUE, overwrite = TRUE)
+  ## German auxilliary data
+  german_administrative_changes(file.path(raw, "ref-kreise-umrech-2019-1990-2018.xlsx"),
+                                file.path(clean, "correct.csv"))
+  file.copy(file.path(raw, "density.csv"), file.path(clean, "density.csv"),
+            overwrite = TRUE)
+  file.copy(file.path(raw, "shapes"), clean,
+            recursive = TRUE, overwrite = TRUE)
 }
+
+rawdata_to_cleandata("./data/raw", "./data/clean")
