0496c0947a9218d49aa11fd98f1fe7fe141f4211
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-04-14 07:09:35 +0200

    more plots

--- new/testcode.jl
+++ new/testcode.jl
@@ -1,6 +1,5 @@
 using CSV, DataFrames, Turing, StatsBase, Random, Plots, StatsPlots
-using ApproxFun
-using CategoricalArrays, NamedArrays, LaTeXStrings
+using ApproxFun, CategoricalArrays, NamedArrays, LaTeXStrings
 
 include("src/utils.jl")
 include("src/othermodels.jl")
@@ -13,20 +12,29 @@ include("src/fullmodel.jl")
 districts = CSV.read("../data/districts.csv", DataFrame)
 addlrd(districts)
 df = CSV.read("../data/FlowDataGermans.csv", DataFrame)
-df = year(age(df, "30-50"), 2017)
-df = sample_flows(df, 1)
+df = year(age(pos(df), "30-50"), 2017)
+df = sample_flows(df, 0.2)
 df = joinlrd(df, districts)
+
+# plot(plot(out.plt[3], title = "No coefs on frompop and topop"),
+#      plot(out2.plt[3], title = "coefs on frompop and topop"),
+#      size = (900, 700), layout = (2, 1))
 
 mdat = gen_mdat(df, districts; distscale = 100.0, ndc = 1, ngc = 1)
 out = estimate(distonly, mdat)
 out2 = estimate(gravity, mdat)
-out3 = estimate(full, mdat)
-out.out
 
 df2 = DataFrame(flows = mdat.flows, our = out.preds, gravity = out2.preds,
                dist = mdat.dist)
+plot(plot(plotdist(df2.flows, df2.our, df2.dist), title = "Our model"),
+     plot(plotdist(df2.flows, df2.gravity, df2.dist), title = "Gravity"),
+     layout = (2, 1))
 
-df2.res = df2.flows ./ df2.preds
-
-plotdist(df2.flows, df2.our, df2.dist)
-plotdist(df2.flows, df2.gravity, df2.dist, true)
+# dfs = df2[StatsBase.sample(1:nrow(df2), 2000), :]
+# plot(xlab = "Distance in km", ylab = L"\log(y / \hat{y})")
+# alpha = .2
+# hline!([0], color = :darkred, linewidth = 2, label = "")
+# scatter!(dfs.dist, log.(dfs.flows ./ dfs.our), label = "Our model",
+#         alpha = alpha)
+# scatter!(dfs.dist, log.(dfs.flows ./ dfs.gravity), label = "gravity",
+#         alpha = alpha)
