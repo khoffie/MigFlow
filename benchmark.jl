ca83be4cca3e8fa2adee6935a93db7fe0c4b95b8
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-04-15 17:43:01 +0200

    renamed file

--- benchmark.jl
+++ benchmark.jl
@@ -10,6 +10,7 @@ include("src/diag.jl")
 include("src/diagplots.jl")
 include("src/fullmodel.jl")
 
+sizemb(x) =  Base.summarysize(x) / 1024^2
 
 function load_data(a, y, p)
     di = CSV.read("data/districts.csv", DataFrame)
@@ -41,13 +42,30 @@ end
 plot_time(ncoefs, b) = _plot_time(plot, ncoefs, b)
 plot_time!(ncoefs, b) = _plot_time(plot!, ncoefs, b)
 
-ndc = [1, 4, 8, 16]
-ngc = [1, 1, 1, 1]
-b01 = benchmark_model(load_data("30-50", 2017, 0.1), ndc, ngc)
-plot_time(ncoefs, b01)
+# ndc = [1, 4, 8, 16]
+# ngc = [1, 1, 1, 1]
+# b01 = benchmark_model(load_data("30-50", 2017, 0.1), ndc, ngc)
+# plot_time(ncoefs, b01)
 
 mdat = gen_mdat(load_data("30-50", 2017, 0.1);
-                distscale = 100.0, ndc = 4, ngc = 16)
+                distscale = 100.0, ndc = 1, ngc = 36)
 out = @time estimate(full, mdat);
-out.plt[5]
+out2 = @time estimate(distonly, mdat);
+out2.out
+out.out[1:5]
+out.plt[3]
 out.plt[6]
+
+plot(1:36, out.out[6 : end-1], title = "Geocheby coefs, scaled as coef / 100")
+scatter!(1:36, out.out[6 : end-1])
+
+
+# out.plt[5]
+# out.plt[6]
+
+# data = load_data("30-50", 2017, 0.1)
+# data
+# Base.summarysize(data)
+# df = CSV.read("data/FlowDataGermans.csv", DataFrame)
+# Base.summarysize(data) / Base.summarysize(df)
+# Base.summarysize(data)
