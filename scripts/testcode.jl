174e7dae2d0345f1e7f105a6959e1a153abdabe1
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-05-17 10:32:24 +0200

    testing

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -12,35 +12,12 @@ include("../src/gen_mdat.jl")
 ## available models
 include("../src/choiceset.jl")
 include("../src/norm.jl")
+include("../src/normalize.jl")
 include("../src/fullmodel.jl")
 include("../src/fullmodel2.jl")
 include("../src/othermodels.jl")
 
-data = load_data("30-50", 2017, 1.0, "../data/"; positive =  false, full = true);
-
-from = sort(unique(data.dffull.fromdist))
-to = sort(unique(data.dffull.todist))
-df = DataFrame(; data.dffull.fromdist, data.dffull.todist, data.dffull.dist)
-distmat = reshape(df.dist, 400, 401)' ./ 100.0
-P = log.(data.districts.pop ./ 153000)
-to = levelcode.(categorical(data.dffull.todist))
-
-desmat = [exp(desirability(P[i], distmat[i, j], 0.15, 0.14, 4)) for i in 1:401, j in 1:400]
-denom = [sum(desmat[i,j] for j in 1:400) for i in 1:401]
-
-
-
-
-[P[i] + log(distmat[i, j]^-4) for i in 1:401, j in 1:400]
-
-
-distmat[1, :]
-distmat[1, :] .== df[df.fromdist .== 1001, :].dist
-
-
-desirability(P, D, ϕ, δ, γ) = P + log((ϕ + (1 - ϕ) / (D + δ)^γ))
-
-
+data = load_data("30-50", 2017, 0.1, "../data/"; positive =  false, full = true);
 
 out1 = @time estimate(distonly, data);
 out1.out
@@ -55,9 +32,6 @@ mdat = gen_mdat("30-50", 2017, 0.1, "../data"; type = "joint",
                 distscale = 100.0, ndc = 28, ngc = 1, only_positive = true);
 out1 = @time estimate(distonly, mdat);
 out1.out
-
-
-
 out1 = @time estimate(norm, mdat);
 out1.out
 out4 = @time estimate(choice, mdat; normalize = false);
@@ -73,21 +47,3 @@ out1.out
 out2.out
 out3.out
 out4.out
-
-plotfit(mdat.flows, out1.preds)
-mdat.flows
-smoother!(log.(out1.preds), log.(mdat.flows))
-
-
-x = log.(out1.preds)
-y = log.(mdat.flows)
-mod = loess(x, y)
-us = range(extrema(x)..., step = .01)
-Loess.predict(mod, us)
-
-plot(out1.plt[1], out3.plt[1])
-
-sum(logpdf.(Poisson.(preds), flows))
-out1.out
-sum(logpdf.(Poisson.(out3.preds), mdat.flows))
-out3.out
