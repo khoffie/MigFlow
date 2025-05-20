7e2251ae6f683dde0f9598176396707c58090798
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-05-20 15:08:04 +0200

    testing

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -17,7 +17,9 @@ include("../src/fullmodel2.jl")
 include("../src/othermodels.jl")
 
 data = load_data("30-50", 2017, 0.1, "../data/"; only_positive = true);
-mdat = gen_mdat(data; type = "joint", distscale = 100.0, ndc = 28, ngc = 1);
+mdat = gen_mdat(data; type = "conditional", distscale = 100.0, ndc = 28, ngc = 1);
+out1 = @time estimate(distonly, mdat);
+out1.out
 out1 = @time estimate(norm, mdat);
 out1.out
 
