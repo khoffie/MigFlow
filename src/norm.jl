9fc2fd2329ad41d4e8c88836d20cccbd4f4ee4c4
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-05-21 09:32:01 +0200

    indexing fixed

--- src/norm.jl
+++ src/norm.jl
@@ -5,21 +5,22 @@ function norm(data::NamedTuple; normalize = true, type)
 
     flows    = df.flows
     fromdist = lc(df.fromdist)
+    todist = lc(df.todist)
     if type == "joint"
         fp = df.frompop
     elseif type == "conditional"
         df2 = combine(groupby(data.df, :fromdist), :flows => sum)
         fp = leftjoin(df, df2, on = :fromdist).flows_sum
     end
-    tp       = df.topop ./ 153000 # median topop
+    P       = districts.pop ./ 153000 # median topop
     di       = df.dist  ./ ds
     nfrom    = length(unique(fromdist))
     N        = length(flows)
-    fpt      = districts.pop
     radius   = fradius.(districts.pop, districts.density)
     data = (; flows, df.fromdist, df.todist, df.dist, df.frompop, df.topop)
-    @model function model(flows, fromdist, fp, tp, di, nfrom, N,
-                          fpt, radius, normalize)
+
+    @model function model(flows, fromdist, todist,fp, P, di, nfrom, N,
+                          radius, normalize)
         a      ~ Normal(-5, 1)
         b  ~ Gamma(1, 1);     ## b  = b_raw / 100
         c_raw  ~ Gamma(15, 0.2);  c  = c_raw / 10
@@ -32,10 +33,10 @@ function norm(data::NamedTuple; normalize = true, type)
         preds = Vector{T}(undef, N)
 
         @inbounds for i in 1:N
-            att[i] = tp[i] * (l + (1-l)/((di[i] + d0)^c))
+            att[i] = P[todist[i]] * (l + (1-l)/((di[i] + d0)^c))
             if normalize
                 denom[fromdist[i]] += att[i]
-                denom[fromdist[i]] += exp(b) * (fpt[fromdist[i]] * (l + (1 - l) /
+                denom[fromdist[i]] += exp(b) * (P[fromdist[i]] * (l + (1 - l) /
                     ((radius[fromdist[i]] + d0)^c)))
             end
         end
@@ -52,8 +53,8 @@ function norm(data::NamedTuple; normalize = true, type)
         return preds
     end
 
-    mdl = model(flows, fromdist, fp, tp, di, nfrom, N,
-                fpt, radius, normalize)
+    mdl = model(flows, fromdist, todist,fp, P, di, nfrom, N,
+                radius, normalize)
     lb = [-20.0, -100.0, 10.0, 0.0, 1.0]
     ub = [20.0, 10.0, 100.0, 99.0, 100.0]
     return (; mdl, lb, ub, data)
