9a4a6aea7b79a89b4b58d47b9cf2af6b3dffd5e8
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-05-21 15:43:20 +0200

    testing

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -18,13 +18,20 @@ include("../src/norm.jl")
 
 data = load_data("30-50", 2017, 0.1, "../data/"; only_positive = true);
 
-out1 = @time estimate(norm, data; norm = "none", type = "conditional");
-out1 = @time estimate(norm, data; norm = "both", type = "conditional");
+out1 = @time estimate(norm, data; norm = "none", type = "conditional",
+                      norm_type = "none");
+
+out1 = @time estimate(norm, data; norm = "both", type = "conditional",
+                      norm_type = "sample");
+
+out1 = @time estimate(norm, data; norm = "both", type = "conditional",
+                      norm_type = "full");
+
 out1 = @time estimate(norm, data; norm = "origin", type = "conditional");
 out1 = @time estimate(norm, data; norm = "destination", type = "conditional");
 
-out1 = @time estimate(norm, data; norm = "none", type = "joint");
-out1 = @time estimate(norm, data; norm = "both", type = "joint");
+out1 = @time estimate(norm, data; norm = "none", type = "joint", norm_type = "none");
+out1 = @time estimate(norm, data; norm = "both", type = "joint", norm_type = "full");
 out1 = @time estimate(norm, data; norm = "origin", type = "joint");
 out1 = @time estimate(norm, data; norm = "destination", type = "joint");
 
