6add073cc6fe5ec4d659163120562094687c4f2f
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-05-27 09:10:24 +0200

    cleanup

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -1,6 +1,6 @@
 using CSV, DataFrames, Turing, StatsBase, Random, Plots, StatsPlots
 using ApproxFun, CategoricalArrays, NamedArrays, LaTeXStrings
-using ADTypes, ReverseDiff, Loess
+using ADTypes, ReverseDiff, Loess, StatProfilerHTML
 
 include("../src/utils.jl")
 include("../src/estimation.jl")
@@ -17,59 +17,18 @@ include("../src/othermodels.jl")
 include("../src/norm.jl")
 
 ratio(out) = exp(out.out["β_raw"] / exp(out.out["α_raw"]))
-data = load_data("30-50", 2017, 1.0, "../data/"; only_positive = true,
-                 seed = 1234);
-
-out1 = @time estimate(norm, data; norm = "none", type = "conditional",
-                      norm_type = "none");
+p = 1.0
 n = "both"
-out2 = @time estimate(norm, data; norm = n, type = "conditional",
-                      norm_type = "sample");
-out3 = @time estimate(norm, data; norm = n, type = "conditional",
-                      norm_type = "full");
-
-data = load_data("30-50", 2017, 0.1, "../data/"; only_positive = true);
-
 ty = "joint"
-out1 = @time estimate(norm, data; norm = "none", type = ty, norm_type = "none");
-n = "both"
-out2 = @time estimate(norm, data; norm = n, type = ty, norm_type = "sample");
-out3 = @time estimate(norm, data; norm = n, type = ty, norm_type = "full");
 
-lc(data.dffull.todist)
+out3f = @profilehtml estimate(norm,
+                              load_data("30-50", 2017, p,
+                                        "data/";
+                                        only_positive = true,
+                                        seed = 1234,
+                                        opf = false);
+                              norm = n, type = ty,
+                              norm_type = "full");
 
-
-out1.out # conditional, no normalization
-out2.out # conditional, normalization with subset
-out3.out # conditional, normalization with full data
-
-out1 = @time estimate(norm, data; norm = "origin", type = "conditional");
-out1 = @time estimate(norm, data; norm = "destination", type = "conditional");
-out1 = @time estimate(norm, data; norm = "both", type = "joint",
-                      norm_type = "sample");
-
-
-
-out1 = @time estimate(norm, data; norm = "none", type = "joint", norm_type = "none");
-out2 = @time estimate(norm, data; norm = "both", type = "joint",
-                      norm_type = "full");
-
-out1.out # no normalization
-out2.out # normalization
-
-
-ratio(out1)
-out1 = @time estimate(norm, data; norm = "origin", type = "joint");
-out1 = @time estimate(norm, data; norm = "destination", type = "joint");
-
-out1 = @time estimate(norm, data; norm = false, type = "joint");
-
-D = data.dffull.dist
-R = fradius.(data.districts.pop, data.districts.density)
-
-D[D .< 30]
-
-density(D)
-density!(R)
-plot(histogram(D), histogram(R))
-exp(-1.3)
+# out1f = @time estimate(norm, data; norm = "none", type = ty, norm_type = "none");
+# out2f = @time estimate(norm, data; norm = n, type = ty, norm_type = "sample");
