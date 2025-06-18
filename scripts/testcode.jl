97d1a8cb9baab1c041a015f0db28f24095f92914
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-06-18 14:24:30 +0200

    cleanup

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -10,19 +10,18 @@ include("../src/diagplots.jl")
 include("../src/chebies.jl")
 include("../src/gen_mdat.jl")
 ## available models
-include("../src/choiceset.jl")
 include("../src/norm.jl")
 
 p = 1.0
 p = .1
 
-data = load_data("30-50", 2017, 1.0,
+data = load_data("30-50", 2017, p,
                  "../data/";
                  only_positive = true,
                  seed = 1234,
                  opf = false);
 
-## out1 = @time estimate(norm, data; ndc = 1, ngc = 1, normalize = true);
+out1 = @time estimate(norm, data; ndc = 1, ngc = 1, normalize = true);
 out2 = @time estimate(norm, data; ndc = 1, ngc = 1, normalize = false);
 
 out1.plt[5]
