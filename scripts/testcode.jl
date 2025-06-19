6ade833ef496d1238e0c1f60d6dcc403bd3d5a6f
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-06-19 10:19:44 +0200

    testing

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -1,6 +1,6 @@
 using CSV, DataFrames, Turing, StatsBase, Random, Plots, StatsPlots
-using ApproxFun, CategoricalArrays, NamedArrays, LaTeXStrings
-using ADTypes, ReverseDiff, Loess
+using ApproxFun, CategoricalArrays, NamedArrays, LaTeXStrings, Loess
+using ADTypes
 
 include("../src/utils.jl")
 include("../src/estimation.jl")
@@ -14,10 +14,17 @@ include("../src/norm.jl")
 p = 1.0
 p = .1
 
-out2 = @time estimate(norm,
-                      load_data("30-50", 2017, p,
-                                "../data/";
-                                only_positive = true,
-                                seed = 1234,
-                                opf = false);
-                      ndc = 28, ngc = 28, normalize = false);
+out = @time estimate(norm,
+                     load_data("30-50", 2017, p,
+                               "../data/";
+                               only_positive = true,
+                               seed = 1234,
+                               opf = false);
+                     ndc = 1,
+                     ngc = 1,
+                     normalize = true);
+
+# LP with 15 geo -33208.7, with exp(Gto - Gfrom)
+out.out
+out.plt[5]
+out.plt[6]
