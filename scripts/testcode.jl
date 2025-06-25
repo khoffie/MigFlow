d779f4c47dd8d8e8d53d06dfc5623a6198a35278
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-06-25 10:04:19 +0200

    nothing

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -17,10 +17,9 @@ p = .1
 data = load_data("30-50", 2016, p, "../data/"; only_positive = true,
                  seed = 1234, opf = true);
 
-out = @time estimate(norm, data, ndc = 4, ngc = 1, normalize = false);
+out = @time estimate(norm, data, ndc = 1, ngc = 4, normalize = false);
 
 # LP with 15 geo -33208.7, with exp(Gto - Gfrom)
 out.out
 out.plt[5]
 out.plt[6]
-
