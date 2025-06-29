e4c38e02f836ebc85eae2505b503dff2f8e99e2b
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-06-29 16:30:21 +0200

    specifying types

--- src/norm.jl
+++ src/norm.jl
@@ -29,10 +29,15 @@ function norm(data::NamedTuple; ndc = 1, ngc = 1, normalize = true, ds = 100)
     data       = (; Y, D, from, to, A,  P, districts.distcode, poporig,
                   ndc, ngc, Rmin, Rmax, xcoord, ycoord, age, year)
 
-    @model function model(Y, from, to, A, P, D, R, Ndist, N, radius,
-                          xcoord, ycoord, xmin, xmax, ymin, ymax, Rmin, Rmax,
-                          fromfull, tofull, Dfull, Nfull, ndc, ngc,
-                          normalize)
+    @model function model(Y::Vector{Int}, from::Vector{Int}, to::Vector{Int},
+                          A::Vector{Int}, P::Vector{Float64}, D::Vector{Float64},
+                          R::Vector{Float64}, Ndist::Int, N::Int, radius::Vector{Float64},
+                          xcoord::Vector{Float64}, ycoord::Vector{Float64},
+                          xmin::Float64, xmax::Float64, ymin::Float64, ymax::Float64,
+                          Rmin::Float64, Rmax::Float64,
+                          fromfull::Vector{Int}, tofull::Vector{Int},
+                          Dfull::Vector{Float64}, Nfull::Int, ndc::Int, ngc::Int,
+                          normalize::Bool)
 
         α_raw  ~ Normal(-5, 1);   α = α_raw
         β_raw  ~ Gamma(1, 1);     β = β_raw
@@ -41,8 +46,7 @@ function norm(data::NamedTuple; ndc = 1, ngc = 1, normalize = true, ds = 100)
         δ_raw  ~ Gamma(10, 1.0);  δ = δ_raw / 100
         ζ_raw ~ StMvN(ndc, 10);   ζ = ζ_raw / 100
         η_raw ~ StMvN(ngc, 10);   η = η_raw / 100
-        # ζ[1] = 0 # not enough for centered data, centering in evaldensitycheby
-        # η[1] = 0 evaldensitycheby(vcat(0.0, fill(20.0, 5)), Rmin, Rmax)
+
         T = eltype(γ)  # to get dual data type for AD
         denom = zeros(T, Ndist)
         ps = Vector{T}(undef, N)
