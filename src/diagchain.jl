5bf46ac55ed926473a821a00806d09632b60aefb
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-03 10:23:08 +0200

    diagchain() plots reordered

--- src/diagchain.jl
+++ src/diagchain.jl
@@ -1,13 +1,4 @@
 function chaindiag(chain, mdl, data)
-    chn[:lp][end]
-    p1 = plot(chn[:lp], label = "$(round(chn[:lp][end], digits = 2))")
-
-    denscoefs = extract_coefs(chn[end, :, 1], "ζ")
-    mat, p2 = plotdensrbf(denscoefs, mdl.data)
-
-    geocoefs = extract_coefs(chn[end, :, 1], "η")
-    dfgeo, p3 = plotgeorbf(geocoefs, mdl.data)
-
     df = DataFrame(;
                    data.df.fromdist,
                    data.df.todist,
@@ -15,15 +6,20 @@ function chaindiag(chain, mdl, data)
                    preds = returned(mdl.mdl, chn[end])[1],
                    data.df.dist);
 
-    p4 = plot(plotfit(df.flows, df.preds),
+    chn[:lp][end]
+    p1 = plot(chn[:lp], label = "$(round(chn[:lp][end], digits = 2))")
+    p2 = plot(plotfit(df.flows, df.preds),
               title = "Cor: $(corround(log.(df.flows), log.(df.preds)))")
-
-    p5 = plotdist(df, :preds)
-
+    p3 = plotdist(df, :preds)
     net = calc_net_df(df);
-    p6 = plot(plotnet(net), title = "cor: $(corround(net.nmr, net.nmrp))")
+    p4 = plot(plotnet(net), title = "cor: $(corround(net.nmr, net.nmrp))")
+    denscoefs = extract_coefs(chn[end, :, 1], "ζ")
+    mat, p5 = plotdensrbf(denscoefs, mdl.data)
+    geocoefs = extract_coefs(chn[end, :, 1], "η")
+    dfgeo, p6 = plotgeorbf(geocoefs, mdl.data)
+
     plts = [p1, p2, p3, p4, p5, p6]
-    return (; df, net, plts)
+    return (; df, net, mat, plts)
 end
 
 function extract_coefs(chn, string)
