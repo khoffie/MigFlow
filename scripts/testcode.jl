66baf33eacaa3356fb2e16d50bc7afd89f0a001a
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-03 08:37:43 +0200

    moved diagrbf plots

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -12,31 +12,6 @@ include("../src/chebies.jl")
 include("../src/norm.jl")
 include("../src/rbf.jl")
 
-function plotgeo(coefs, districts)
-    xcoord     = scale_to_unit(districts.xcoord)
-    ycoord     = scale_to_unit(districts.ycoord)
-    xmin, xmax = extrema(xcoord)
-    ymin, ymax = extrema(ycoord)
-    cxgeo      = [range(-0.6, 0.4, Int(sqrt(length(coefs))));]
-    cygeo      = [range(-0.9, 0.6, Int(sqrt(length(coefs))));]
-    geoscale   = rbfscale(cxgeo, cygeo, 1.0)
-    geo = [interpolant(rbf, xcoord[i], ycoord[i], coefmat(coefs),
-                       cxgeo, cygeo, geoscale) for i in eachindex(xcoord)];
-    geo = geo .- mean(geo)
-    dfgeo = DataFrame(; xcoord, ycoord, geo)
-    ratio = (ymax - ymin) / (xmax - xmin)
-    width = 600
-    p = scatter(xcoord, ycoord,
-                marker_z = dfgeo.geo,
-                markersize = 10, size = (600, width * ratio),
-                label = "")
-    res = collect(product(cxgeo, cygeo))
-    xvals = getindex.(res, 1)
-    yvals = getindex.(res, 2)
-    scatter!(vec(xvals), vec(yvals), color = :red)
-    display(p)
-    return df
-end
 
 function extract_coefs(chn, string)
     nms = String.(names(chn))
@@ -44,21 +19,6 @@ function extract_coefs(chn, string)
 end
 
 corround(x, y) = round(cor(x, y), digits = 2)
-
-function heat(coefs, k, districts)
-    R = scale_to_unit(log.(districts.density ./ median(districts.density)))
-    Rmin, Rmax = extrema(R)
-    vals = range(Rmin, Rmax, 1000)
-    s = Int(sqrt(length(coefs)))
-    cx = [range(Rmin, Rmax, s);]
-    cy = [range(Rmin, Rmax, s);]
-    scale = rbfscale(cx, cy, k)
-    mat = [interpolant(rbf, Rfrom, Rto, coefmat(coefs), cx, cy, scale)
-           for Rfrom in vals, Rto in vals]';
-    mat = mat .- mean(mat)
-    display(heatmap(mat))
-    return mat
-end
 
 p = 1.0
 p = 0.1
@@ -72,17 +32,25 @@ out = @time estimate(norm, data;
 
 AD = AutoEnzyme()
 AD = ADTypes.AutoForwardDiff()
-mdl = norm(data; densscale = 1.5, ndc = 4, ngc = 4, normalize = false);
-mdl = dist(data; densscale = 1.5, ndc = 4, ngc = 4, normalize = false);
-mdl = distdense(data; densscale = 1.5, ndc = 4, ngc = 4, normalize = false);
+mdl = norm(data; ndc = 4, ngc = 4, normalize = false);
+chn = Turing.sample(mdl.mdl, NUTS(10, .7), 1, progress = true)
 
-mdl = distgeo(data; densscale = 1.5, ndc = 4, ngc = 4, normalize = false);
+## tree depth 4 or 5
+## check how NUTS works
+## rescale y axis, such that 1m = 1m
 chn = Turing.sample(mdl.mdl, NUTS(100, .7), 10, progress = true)
-
-
+chn
+chn[:lp][end]
 plot(chn[:lp], label = "$(round(chn[:lp][end], digits = 2))")
+
 denscoefs = extract_coefs(chn[end, :, 1], "ζ")
+plotdensrbf(denscoefs, mdl.data)
+
 geocoefs = extract_coefs(chn[end, :, 1], "η")
+plotgeorbf(geocoefs, mdl.data)
+
+heat(denscoefs, 1.5, districts)
+
 
 preds = returned(mdl.mdl, chn[end])[1];
 df = DataFrame(; data.df.fromdist, data.df.todist, data.df.flows, preds, data.df.dist);
@@ -94,15 +62,9 @@ plotdist(df, :preds)
 net = calc_net_df(df);
 plot(plotnet(net), title = "cor: $(corround(net.nmr, net.nmrp))")
 
-districts = year(CSV.read("../data/districts.csv", DataFrame), 2017)
-mat = heat(denscoefs, districts);
-
+# districts = year(CSV.read("../data/districts.csv", DataFrame), 2017)
 w = zeros(4, 4)
-w[3, 3] = 2
-w[1, 1] = 1
 w[4, 1] = 1
-
-coefmat(w)
-heat(w, 1.0, districts);
-
-plotgeo(w, districts)
+w[1, 1] = 3
+# heat(w, 1, districts)
+plotgeo(w, 1.5, districts)
---
+++ src/diagrbf.jl
@@ -0,0 +1,61 @@
+function plotgeorbf(coefs::Vector{Float64},
+                    data::NamedTuple)
+    xcoord = data.xcoord
+    ycoord = data.ycoord
+    cx = data.cxgeo
+    cy = data.cygeo
+    k = data.kgeo
+    return plotgeorbf_(coefs, xcoord, ycoord, cx, cy, k)
+end
+
+function plotgeorbf_(coefs::Vector{Float64},
+                    xcoord::Vector{Float64},
+                    ycoord::Vector{Float64},
+                    cx::Vector{Float64},
+                    cy::Vector{Float64},
+                    k::Float64)
+    xmin, xmax = extrema(xcoord)
+    ymin, ymax = extrema(ycoord)
+    geoscale   = rbfscale(cx, cy, k)
+    geo = [interpolant(rbf, xcoord[i], ycoord[i], coefmat(coefs ./ 100),
+                       cx, cy, geoscale) for i in eachindex(xcoord)];
+    geo = geo .- mean(geo)
+    dfgeo = DataFrame(; xcoord, ycoord, geo)
+    ratio = (ymax - ymin) / (xmax - xmin)
+    ratio = 1.33
+    width = 600
+    p = scatter(xcoord, ycoord,
+                marker_z = dfgeo.geo,
+                markersize = 10, size = (600, width * ratio),
+                label = "", yflip = false)
+    res = collect(IterTools.product(cx, cy))
+    xvals = getindex.(res, 1)
+    yvals = getindex.(res, 2)
+    scatter!(vec(xvals), vec(yvals), color = :red)
+    display(p)
+    return dfgeo
+end
+
+function plotdensrbf(coefs::Vector{Float64}, data::NamedTuple)
+    cx = data.cx
+    cy = data.cy
+    k = data.kdens
+    R = data.R
+    return plotdensrbf_(coefs, cx, cy, k, R)
+end
+
+function plotdensrbf_(coefs::Vector{Float64},
+                      cx::Vector{Float64},
+                      cy::Vector{Float64},
+                      k::Float64,
+                      R::Vector{Float64})
+    Rmin, Rmax = extrema(R)
+    vals = range(Rmin, Rmax, 1000)
+    s = Int(sqrt(length(coefs)))
+    scale = rbfscale(cx, cy, k)
+    mat = [interpolant(rbf, Rfrom, Rto, coefmat(coefs), cx, cy, scale)
+           for Rfrom in vals, Rto in vals]';
+    mat = mat .- mean(mat)
+    display(heatmap(mat))
+    return mat
+end
