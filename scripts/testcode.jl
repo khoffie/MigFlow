ab22e24979f66ed19309a1428d991fb4e4d24e70
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-04 13:18:26 +0200

    testing

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -1,14 +1,16 @@
 using CSV, DataFrames, Turing, StatsBase, Random, Plots, StatsPlots
 using ApproxFun, CategoricalArrays, NamedArrays, LaTeXStrings, Loess
 using ADTypes, KernelDensity, Serialization, DynamicPPL, LinearAlgebra
-using BenchmarkTools, IterTools
-
+using BenchmarkTools, IterTools, StatProfilerHTML, ReverseDiff
+## using Enzyme
 include("../src/utils.jl")
 include("../src/estimation.jl")
 include("../src/loadgermdata.jl")
 include("../src/diag.jl")
 include("../src/diagplots.jl")
 include("../src/chebies.jl")
+include("../src/diagchain.jl")
+include("../src/diagrbf.jl")
 include("../src/norm.jl")
 include("../src/rbf.jl")
 
@@ -19,18 +21,23 @@ p = 0.1
 data = load_data("18-25", 2017, p, "../data/"; only_positive = true,
                  seed = 1234, opf = false);
 
-AD = AutoEnzyme()
-mdl = norm(data; ndc = 9, ngc = 4, normalize = false);
-chn = Turing.sample(mdl.mdl, NUTS(100, .7), 10, progress = true)
+AD = AutoForwardDiff()
+## AD = AutoReverseDiff()
+## AD = AutoEnzyme()
+mdl = norm(data;
+           kdens = 2.0,
+           kgeo = 2.0,
+           ndc = 4, ngc = 4,
+           normalize = false);
+
+Random.seed!(1234)
+chn = Turing.sample(mdl.mdl, NUTS(100, .7; adtype = AD), 1, progress = true)
 out = chaindiag(chn, mdl, data);
 
-out.plts[1]
-out.plts[2]
-out.plts[3]
-out.plts[4]
-out.plts[5]
-out.plts[6]
-
+# plot(out.plts[1:4]...)
+# out.plts[5]
+# out.plts[6]
+# chn
 
 ## tree depth 4 or 5
 ## check how NUTS works
