3b21daf360b6b62def663f914b7c6e257d1b4489
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-08 15:12:26 +0200

    fit_years()

--- scripts/testmodels.jl
+++ scripts/testmodels.jl
@@ -41,28 +41,74 @@ function postprocess(results, loopvec, lpthresh = -10e6)
     return (; df, pls)
 end
 
+rbfinits(N, σ, t = 90.0) = clamp.(rand(MvNormal(zeros(N), σ^2 *I(N))), -t, t)
 
+function fit_years(a, p)
+    years = vcat(2000:2002, 2004:2017)
+    results = NamedTuple[]
+    inits = [-7.5, 18.0, 20.0, rbfinits(9, 40.0)..., rbfinits(8, 10.0)...]
+    for y in years
+
+        data = load_data(a, y, p, "../data/"; only_positive = true,
+                         seed = 1234, opf = false);
+        mdl = norm(data, normalize = false, ndc = 9, ngcx = 2);
+        out = @time estimate(mdl, optim_kwargs = (; show_trace = false,
+                                                  inits = inits));
+        println("$y done")
+        push!(results, out)
+    end
+    serialize("output/optim$(a)", results)
+    return results
+end
+
+results = fit_years("18-25", 1.0)
+
+
+
+out = postprocess(results, vcat(2000:2002, 2004:2017), -200e3);
+out = postprocess(results, vcat(2000:2002, 2004:2017));
+out.df
+plot(results[3].plts[1:4]...)
+out
+results[2].plts[5]
+
+plot(out.pls[1:3]...)
+plot(out.pls[4][1:6]...)
+plot(out.pls[4][7:13]...)
+
+plot(out.pls[5][1:6]...)
+plot(out.pls[5][1:6]...)
+out.df[1, 6: 14]
+
+ws = [coefmat(collect(out.df[i, 6:14])) for i in 1 : nrow(out.df)]
+ws[1]
+ws[2]
+names(out.df)
+out.df[12, 6:14]
+out.df[12, 15:22]
+
+ws = [coefmat(collect(out.df[i, 15 : 22])) for i in 1 : nrow(out.df)]
+ws[8]
 
 p = 1.0
-p = 0.1
+p = 0.5
 data = load_data("18-25", 2017, p, "../data/"; only_positive = true,
                  seed = 1234, opf = false);
 mdl = norm(data, normalize = false, ndc = 9, ngcx = 2);
-out = estimate(mdl, optim_kwargs = (; show_trace = true));
 
-years = vcat(2000:2002, 2004:2017)
-results = NamedTuple[]
-for y in years
-    p = 1.0
-    data = load_data("18-25", y, p, "../data/"; only_positive = true,
-                     seed = 1234, opf = false);
-    mdl = norm(data, normalize = false, ndc = 9, ngcx = 2);
-    out = estimate(mdl, optim_kwargs = (; show_trace = false));
-    println("$y done")
-    push!(results, out)
-end
 
-serialize("output/optim18-25", results)
+out = estimate(mdl, optim_kwargs = (; show_trace = false,
+                                    inits = inits));
+plot(out.plts[1:4]...)
+res1 = out
+res1.chn
+
+
+
+mles = try_optim_once(mdl.mdl, mdl.lb, mdl.ub)
+mles.lp
+
+runoptim(mdl.mdl, mdl.lb, mdl.ub; lp_thresh = -1)
 
 seeds = rand(1:1000, 10)
 
@@ -77,18 +123,23 @@ for s in seeds
     push!(results, out)
 end
 
-out = postprocess(results, seeds, -50e3)
+out = postprocess(results, seeds, -200e3)
+out
+plot(results[5].plts[1:4]...)
 plot(out.pls[1:3]...)
-plot(out.pls[4][1:3]...)
-plot(out.pls[5][1:3]...)
+
+plot(out.pls[4][1:6]...)
+plot(out.pls[5][1:3]..., layout = (3, 1))
 
 
 p = .1
 data = load_data("18-25", 2017, p, "../data/"; only_positive = true,
                  seed = seeds[1], opf = false);
 mdl = norm(data, normalize = false, ndc = 9, ngcx = 2);
-out = estimate(mdl)
+out = @time estimate(mdl, optim_kwargs = (; lp_thresh = -5000))
 
+
+mdl
 out2 = estimate(mdl, optim_kwargs = (; reltol = 1e-10))
 out2.chn
 out.chn
