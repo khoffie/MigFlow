8af883d102f441b3976bc2c987d8fab9459ba649
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-09 11:15:25 +0200

    renamed

--- scripts/fitgroups.jl
+++ scripts/fitgroups.jl
@@ -40,19 +40,49 @@ function postprocess(results, loopvec, lpthresh = -10e6)
 end
 
 rbfinits(N, σ, t = 90.0) = clamp.(rand(MvNormal(zeros(N), σ^2 *I(N))), -t, t)
-inits = [-7.5, 18.0, 20.0, rbfinits(9, 40.0)..., rbfinits(8, 10.0)...]
+
+
+function initialize(a, ndc, ngcx, ngcy)
+    density = rbfinits(ndc, 40.0)
+    geo = rbfinits(ngcx * ngcy, 10.0)
+    a == "below18" && return [-8.0, 25.0, 30.0, density..., geo...]
+    a == "18-25" && return [-7.0, 18.0, 20.0, density..., geo...]
+    a == "25-30" && return [-7.0, 18.0, 20.0, density..., geo...]
+    a == "30-50" && return [-7.5, 23.0, 30.0, density..., geo...]
+    a == "50-65" && return [-7.5, 23.0, 30.0, density..., geo...]
+    a == "above65" && return [-7.5, 23.0, 30.0, density..., geo...]
+end
+
+function bound(a, ndc, ngcx, ngcy)
+    lbdensity = fill(-100.0, ndc)
+    lbgeo = fill(-100.0, ngcx * ngcy)
+    ubdensity = fill(100.0, ndc)
+    ubgeo = fill(100.0, ngcx * ngcy)
+
+    pastelb(c) = vcat(c, lbdensity..., lbgeo...)
+    pasteub(c) = vcat(c, ubdensity..., ubgeo...)
+    ## ub alpha only makes sense for distscale = 100 and pop /
+    ## median(pop). Otherwise base prob to migrate might be very different
+    a == "below18" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 50.0])
+    a == "18-25" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 30.0])
+    a == "25-30" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 30.0])
+    a == "30-50" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 40.0])
+    a == "50-65" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 40.0])
+    a == "above65" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 40.0])
+end
 
 function fit_years(a, p)
     years = vcat(2000:2002, 2004:2017)
     results = NamedTuple[]
-    inits = [-7.5, 18.0, 20.0, rbfinits(9, 40.0)..., rbfinits(8, 10.0)...]
-    for y in years
-        mdl = norm(load_data(a, y, p, "../data/";
+
+    Threads.@threads for y in years
+        mdl = norm(load_data(a, 2014, p, "../data/";
                              only_positive = true,
                              seed = 1234, opf = false),
                    normalize = false, ndc = 9, ngcx = 2);
-        out = @time estimate(mdl, optim_kwargs = (; show_trace = false,
-                                                  inits = inits));
+        inits = initialize(mdl.data.age, mdl.mdl.args.ndc, mdl.mdl.args.ngcx, mdl.mdl.args.ngcy);
+        out = @time estimate(mdl, optim_kwargs = (; show_trace = false, inits = inits));
+
         println("$y done")
         push!(results, out)
         serialize("output/optim$(a)", results)
