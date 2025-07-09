ef1c07655d4d18d3ed166cb481405eb312d692d7
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-09 11:15:37 +0200

    mv code to fitgroups.jl

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -32,33 +32,9 @@ mdl = norm(data,
 
 out = @time estimate(mdl, optim_kwargs = (; show_trace = false));
 
-rbfinits(N, σ, t = 90.0) = clamp.(rand(MvNormal(zeros(N), σ^2 *I(N))), -t, t)
 
 
-function initialize(a, ndc, ngcx, ngcy)
-    density = rbfinits(ndc, 40.0)
-    geo = rbfinits(ngcx * ngcy, 10.0)
-    a == "below18" && return [-8.0, 25.0, 30.0, density..., geo...]
-    a == "18-25" && return [-7.0, 18.0, 20.0, density..., geo...]
-    a == "25-30" && return [-7.0, 18.0, 20.0, density..., geo...]
-    a == "30-50" && return [-7.5, 23.0, 30.0, density..., geo...]
-    a == "50-65" && return [-7.5, 23.0, 30.0, density..., geo...]
-    a == "above65" && return [-7.5, 23.0, 30.0, density..., geo...]
-end
-
-function bound(a, ndc, ngcx, ngcy)
-    lbdensity = fill(-100.0, ndc)
-    lbgeo = fill(-100.0, ngcx * ngcy)
-    ubdensity = fill(100.0, ndc)
-    ubgeo = fill(100.0, ngcx * ngcy)
-
-    pastelb(c) = vcat(c, lbdensity..., lbgeo...)
-    pasteub(c) = vcat(c, ubdensity..., ubgeo...)
-
-    a == "below18" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 50.0])
-    a == "18-25" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 30.0])
-    a == "25-30" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 30.0])
-    a == "30-50" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 40.0])
-    a == "50-65" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 40.0])
-    a == "above65" && return pastelb([-10.0, 10.0, 1.0]), pasteub([-5.0, 40.0, 40.0])
-end
+out.chn
+plot(out.plts[1:4]...)
+plot(out.plts[5])
+plot(out.plts[6])
