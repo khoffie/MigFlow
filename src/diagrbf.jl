26660a091899ec6c0696dab8b0373aaad60363ab
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-10 19:23:42 +0200

    rbf plots with clim and take (mdl, chn) as input

--- src/diagrbf.jl
+++ src/diagrbf.jl
@@ -1,18 +1,18 @@
-function plotgeorbf(n::NamedTuple)
+function plotgeorbf(n::NamedTuple, clim = nothing)
     geocoefs = extract_coefs(n.chn[end, :, 1], "η")
     data = n.mdl.mdl.args
     y = n.chn[:year][1]
-    return plotgeorbf(geocoefs, data)
+    return plotgeorbf(geocoefs, data, "Year $y", clim)
 end
 
 function plotgeorbf(coefs::Vector{Float64},
-                    data::NamedTuple, title)
+                    data::NamedTuple, title, clim)
     xcoord = data.xcoord
     ycoord = data.ycoord
     cx = data.cxgeo
     cy = data.cygeo
     scale = data.geo_scale
-    return plotgeorbf_(coefs, xcoord, ycoord, cx, cy, scale)
+    return plotgeorbf_(coefs, xcoord, ycoord, cx, cy, scale, title, clim)
 end
 
 function plotgeorbf_(coefs::Vector{Float64},
@@ -21,7 +21,8 @@ function plotgeorbf_(coefs::Vector{Float64},
                      cx::Vector{Float64},
                      cy::Vector{Float64},
                      scale::Float64,
-                     title::String)
+                     title::String,
+                     clim)
     xmin, xmax = extrema(xcoord)
     ymin, ymax = extrema(ycoord)
     geo = [interpolant(rbf, xcoord[i], ycoord[i],
@@ -35,7 +36,9 @@ function plotgeorbf_(coefs::Vector{Float64},
     p = scatter(xcoord, ycoord,
                 marker_z = dfgeo.geo,
                 markersize = 10, size = (600, width * ratio),
-                label = "", yflip = false, title = title)
+                label = "", yflip = false,
+                title = title,
+                clim = clim)
     res = collect(IterTools.product(cx, cy))
     xvals = getindex.(res, 1)
     yvals = getindex.(res, 2)
@@ -43,25 +46,33 @@ function plotgeorbf_(coefs::Vector{Float64},
     return dfgeo, p
 end
 
-function plotdensrbf(coefs::Vector{Float64}, data::NamedTuple)
+function plotdensrbf(n::NamedTuple, clim)
+    coefs = extract_coefs(n.chn[end, :, 1], "ζ")
+    data = n.mdl.mdl.args
+    y = n.chn[:year][1]
+    return plotdensrbf(coefs, data, "Year $y", clim)
+end
+
+function plotdensrbf(coefs::Vector{Float64}, data::NamedTuple,
+                     title = nothing, clim = nothing)
     cx = data.cx
     cy = data.cy
     scale = data.rbf_scale
     R = data.R
-    return plotdensrbf_(coefs, cx, cy, scale, R)
+    return plotdensrbf_(coefs, cx, cy, scale, R, title, clim)
 end
 
 function plotdensrbf_(coefs::Vector{Float64},
                       cx::Vector{Float64},
                       cy::Vector{Float64},
                       scale::Float64,
-                      R::Vector{Float64})
+                      R::Vector{Float64}, title, clim)
     Rmin, Rmax = extrema(R)
     vals = range(Rmin, Rmax, 1000)
     s = Int(sqrt(length(coefs)))
     mat = [interpolant(rbf, Rfrom, Rto, coefmat(coefs ./ 10), cx, cy, scale)
            for Rto in vals, Rfrom in vals];
     mat = mat .- mean(mat)
-    p = heatmap(mat)
+    p = plot(heatmap(mat), title = title, clim = clim)
     return mat, p
 end
