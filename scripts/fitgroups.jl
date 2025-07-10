5e54e82be5483c00f015b402c056e931ddab92b4
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-10 09:49:06 +0200

    fitage(), fit30to50() to handle 30to50 separately

--- scripts/fitgroups.jl
+++ scripts/fitgroups.jl
@@ -27,7 +27,7 @@ function initialize(a, ndc, ngcx, ngcy)
     a == "above65" && return [-7.5, 23.0, 30.0, density..., geo...]
 end
 
-function fit_years(a, p)
+function fit_years(a)
     ## 2003 has data issues
     allyears = vcat(2000:2002, 2004:2017)
     if isfile("output/optim$a")
@@ -41,13 +41,11 @@ function fit_years(a, p)
 
     Threads.@threads for y in years
         println("Starting $a in $y")
-        mdl = norm(load_data(a, y, p, "../data/";
-                             only_positive = true,
-                             seed = 1234, opf = false),
-                   normalize = false, ndc = 9, ngcx = 2);
-        inits = initialize(mdl.data.age, mdl.mdl.args.ndc, mdl.mdl.args.ngcx, mdl.mdl.args.ngcy);
-        out = @time estimate(mdl, optim_kwargs = (; show_trace = false, inits = inits));
-
+        if a == "30-50"
+            out = fit30to50(y)
+        else
+            out = fitage(a, y)
+        end
         println("$a in $y done")
         push!(results, out)
         serialize("output/optim$(a)", results)
@@ -55,23 +53,37 @@ function fit_years(a, p)
     return results
 end
 
-ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
-ages = ["30-50"]
-for a in ages
-    fit_years(a, 1.0)
+function fitage(a, y)
+    mdl = norm(load_data(a, y, 1.0, "../data/";
+                         only_positive = true,
+                         seed = 1234, opf = false),
+               normalize = false, ndc = 9, ngcx = 2);
+    inits = initialize(mdl.data.age, mdl.mdl.args.ndc, mdl.mdl.args.ngcx, mdl.mdl.args.ngcy);
+    return @time estimate(mdl, optim_kwargs = (; show_trace = false, inits = inits));
 end
 
-mdl = norm(load_data("30-50", 2017, 0.5, "../data/";
-                     only_positive = true,
-                     seed = 1234, opf = false),
-           normalize = false, ndc = 9, ngcx = 2);
-inits = initialize(mdl.data.age, mdl.mdl.args.ndc, mdl.mdl.args.ngcx, mdl.mdl.args.ngcy);
-out = @time estimate(mdl, optim_kwargs = (; show_trace = false, inits = inits));
+function fit30to50(y)
+    ## 30-50 is harder to fit so we use MLEs from fit of half the
+    ## data, which works better. We maybe could use better inits and
+    ## fit full data right away, but we likely would need very
+    ## specific inits / weights for the RBFs
+    mdl = norm(load_data("30-50", y, 0.5, "../data/";
+                         only_positive = true,
+                         seed = 1234, opf = false),
+               normalize = false, ndc = 9, ngcx = 2);
+    inits = initialize(mdl.data.age, mdl.mdl.args.ndc, mdl.mdl.args.ngcx, mdl.mdl.args.ngcy);
+    out = @time estimate(mdl, optim_kwargs = (; show_trace = false, inits = inits));
 
-mdl = norm(load_data("30-50", 2017, 1.0, "../data/";
-                     only_positive = true,
-                     seed = 1234, opf = false),
-           normalize = false, ndc = 9, ngcx = 2);
-## inits = initialize(mdl.data.age, mdl.mdl.args.ndc, mdl.mdl.args.ngcx, mdl.mdl.args.ngcy);
-inits = out.chn[1, 1 : end - 4, 1].value.data
-out = @time estimate(mdl, optim_kwargs = (; show_trace = false, inits = reshape(inits, 20)));
+    mdl = norm(load_data("30-50", y, 1.0, "../data/";
+                         only_positive = true,
+                         seed = 1234, opf = false),
+               normalize = false, ndc = 9, ngcx = 2);
+    ## inits = initialize(mdl.data.age, mdl.mdl.args.ndc, mdl.mdl.args.ngcx, mdl.mdl.args.ngcy);
+    inits = out.chn[1, 1 : end - 4, 1].value.data
+    return  @time estimate(mdl, optim_kwargs = (; show_trace = false, inits = reshape(inits, 20)));
+end
+
+ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
+for a in ages
+    fit_years(a)
+end
