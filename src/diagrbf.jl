bee62ec61170f841f51895c11b026f3b2df21c32
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-07-23 11:58:00 +0200

    plotting maps with Makie

--- src/diagrbf.jl
+++ src/diagrbf.jl
@@ -1,21 +1,21 @@
-function plotgeorbf(n::NamedTuple, clim = nothing)
+function plotgeorbf(n::NamedTuple, shp::DataFrame, clim = nothing)
     geocoefs = extract_coefs(n.chn[end, :, 1], "η")
     data = n.mdl.mdl.args
     y = n.chn[:year][1]
-    return plotgeorbf(geocoefs, data, "Year $y", clim)
+    return plotgeorbf(geocoefs, data, shp, "Year $y", clim)
 end
 
 function plotgeorbf(coefs::Vector{Float64},
-                    data::NamedTuple, title, clim)
+                    data::NamedTuple, shp::DataFrame, title, clim)
     xcoord = data.xcoord
     ycoord = data.ycoord
     cx = data.cxgeo
     cy = data.cygeo
     scale = data.geo_scale
-    return plotgeorbf_(coefs, xcoord, ycoord, cx, cy, scale, title, clim)
+    return plotgeorbf_(shp, coefs, xcoord, ycoord, cx, cy, scale, title, clim)
 end
 
-function plotgeorbf_(coefs::Vector{Float64},
+function plotgeorbf_(shp::DataFrame, coefs::Vector{Float64},
                      xcoord::Vector{Float64},
                      ycoord::Vector{Float64},
                      cx::Vector{Float64},
@@ -30,20 +30,12 @@ function plotgeorbf_(coefs::Vector{Float64},
                        cx, cy, scale) for i in eachindex(xcoord)];
     geo = geo .- mean(geo)
     dfgeo = DataFrame(; xcoord, ycoord, geo)
-    ratio = (ymax - ymin) / (xmax - xmin)
-    ratio = 1.33
-    width = 600
-    p = scatter(xcoord, ycoord,
-                marker_z = dfgeo.geo,
-                markersize = 10, size = (600, width * ratio),
-                label = "", yflip = false,
-                title = title,
-                clim = clim)
-    res = collect(IterTools.product(cx, cy))
-    xvals = getindex.(res, 1)
-    yvals = getindex.(res, 2)
-    scatter!(vec(xvals), vec(yvals), color = :red)
-    return dfgeo, p
+    dfgeo.xcoord = round.(dfgeo.xcoord, digits = 4)
+    dfgeo.ycoord = round.(dfgeo.ycoord, digits = 4)
+    shp2 = leftjoin(shp, dfgeo, on = [:xcoord, :ycoord])
+    shp2 = GeoTable(shp2)
+    p = viz(shp2.geometry, color = shp2.geo)
+    return dfgeo,  p
 end
 
 function plotdensrbf(n::NamedTuple, clim)
