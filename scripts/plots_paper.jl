5926fafe34b399973fcc8abf022af9f2b80d6dd9
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-04 13:44:45 +0200

    better distance plot

--- scripts/plots_paper.jl
+++ scripts/plots_paper.jl
@@ -211,7 +211,7 @@ m = glm(@formula(flows ~ log(frompop) + log(topop) + log(dist)), df17, Poisson()
 
 ######################################################################
 ########################### Distance #################################
-res, dfres = maincircle(df17, di, false);
+res = maincircle(df17, di, false);
 dfdist = combine(groupby(df17, :dist),
                  [ nrow => :count,
                    :flows => sum => :flows,
@@ -220,63 +220,51 @@ dfdist = combine(groupby(df17, :dist),
                    ])
 res = dropmissing(leftjoin(res, dfdist, on = :radius => :dist))
 
-f = Figure(size = (350, 350), fontsize = 10);
-ax = Axis(f[1, 1],
-          xlabel = "Distance (km)",
-          xgridvisible = false, ygridvisible = false)
-hideydecorations!(ax)
-xs = StatsBase.sample(res.radius, Weights(res.pop), 10^4);
-lines!(ax, kde(xs, Gamma(1, 10)), color = :red, label = "Pop")
+model(res, γ, ϵ) = Float64.(res.radius).^γ .+ ϵ
 
-xs = StatsBase.sample(res.radius, Weights(res.area), 10^4);
-lines!(ax, kde(xs, Gamma(1, 10)), color = :blue, label = "Area")
+function plot(res, γ, ϵ)
+    res.pred = model(res, γ, ϵ)
 
-xs = StatsBase.sample(res.radius, Weights(res.pot), 10^4)
-lines!(ax, kde(xs, Gamma(1, 10)), color = :purple, label = "a2πr")
+    f = Figure(size = (350, 350), fontsize = 10);
+    ax1 = Axis(f[1, 1],
+              xlabel = "Distance (km)",
+              xgridvisible = false, ygridvisible = false)
 
-xs = StatsBase.sample(res.radius, Weights(res.count), 10^4)
-lines!(ax, kde(xs, Gamma(1, 10)), color = :green, label = "Unweighted")
+    xs = StatsBase.sample(res.radius, Weights(res.pop), 10^4);
+    lines!(ax1, kde(xs, Gamma(1, 10)), color = :red, label = "Pop")
+    xs = StatsBase.sample(res.radius, Weights(res.area), 10^4);
+    lines!(ax1, kde(xs, Gamma(1, 10)), color = :blue, label = "Area")
+    xs = StatsBase.sample(res.radius, Weights(res.pot), 10^4)
+    lines!(ax1, kde(xs, Gamma(1, 10)), color = :purple, label = "a2πr")
+    xs = StatsBase.sample(res.radius, Weights(res.count), 10^4)
+    lines!(ax1, kde(xs, Gamma(1, 10)), color = :green, label = "Unweighted")
+    xs = StatsBase.sample(res.radius, Weights(res.flows), 10^4)
+    lines!(ax1, kde(xs, Gamma(1, 10)), color = :yellow, label = "Flows")
+    xs = StatsBase.sample(res.radius, Weights(res.count .* res.pred), 10^4)
+    lines!(ax1, kde(xs, Gamma(1, 10)), color = :brown, label = "radius^$γ + $ϵ")
 
-xs = StatsBase.sample(res.radius, Weights(res.flows), 10^4)
-lines!(ax, kde(xs, Gamma(1, 10)), color = :yellow, label = "Flows")
+    ax2 = Axis(f[1, 2],
+               xlabel = "Distance (km)",
+               xgridvisible = false, ygridvisible = false)
+    res.flows[res.flows .== 0] .= 1
 
-xs = StatsBase.sample(res.radius, Weights(res.radius.^(-1.05)), 10^4)
-lines!(ax, kde(xs, Gamma(1, 10)), color = :orange, label = "Model")
+    xs = StatsBase.sample(res.radius, Weights(res.flows ./ res.flows), 10^4);
+    lines!(ax2, kde(xs, Gamma(1, 10)), color = :red, label = "Flows / Flows")
+    xs = StatsBase.sample(res.radius, Weights(res.flows ./ (res.pred .* res.count)), 10^4);
+    lines!(ax2, kde(xs, Gamma(1, 10)), color = :brown, label = "Flows / (radius^$γ + $ϵ)")
 
-axislegend(ax, framevisible = false, patchsize = (5, 10))
-f
+    hideydecorations!(ax1)
+    hideydecorations!(ax2)
+    axislegend(ax1, framevisible = false, patchsize = (5, 10))
+    axislegend(ax2, framevisible = false, patchsize = (5, 10))
 
-ax2 = Axis(f[1, 2],
-           xlabel = "Distance (km)",
-           xgridvisible = false, ygridvisible = false)
-hideydecorations!(ax2)
+    titlelayout = GridLayout(f[0, 1], halign = :left, tellwidth = false)
+    Label(titlelayout[1, 1], "Movement Distances", halign = :left, fontsize = 15, font = "TeX Gyre Heros Bold Makie")
+    Label(titlelayout[2, 1], "2000-2017, all age groups", halign = :left, fontsize = 10)
+    rowgap!(titlelayout, 0)
 
-xs = StatsBase.wsample(dfres.dist, Weights(dfres.fpp), 10^4)
-lines!(ax2, kde(xs, Gamma(1, 10)), label = L"\text{Adjusted} a2πr", color = :purple)
-xs = StatsBase.wsample(dfres.dist, Weights(dfres.fppop), 10^4)
-lines!(ax2, kde(xs, Gamma(1, 10)), label = L"\text{Adjusted} Pop", color = :green)
-xs = StatsBase.wsample(dfres.dist, Weights(dfres.flows), 10^4)
-lines!(ax2, kde(xs, Gamma(1, 10)), label = L"\text{Observed}", color = :red)
-axislegend(ax2, framevisible = false, patchsize = (5, 10))
+    return res, f
+end
 
-ax3 = Axis(f[1, 3],
-           xlabel = "Distance (km)",
-           ylabel = "Density of log(y)",
-           xgridvisible = false, ygridvisible = false)
-hideydecorations!(ax3)
-lines!(ax3, dfres2.distbin, y2 ./ sum(y2), color = :purple,
-       label = "Adjusted\ny = flows / a2πr"
-       )
-lines!(ax3, dfres2.distbin, y3 ./ sum(abs.(y3)), color = :green,
-       label = "Adjusted\ny = flows / P")
-lines!(ax3, dfres2.distbin, y1 ./ sum(y1), color = :red,
-       label = "Observed\ny = flows")
-axislegend(ax3, framevisible = false, patchsize = (5, 10))
-
-titlelayout = GridLayout(f[0, 1], halign = :left, tellwidth = false)
-Label(titlelayout[1, 1], "Movement Distances", halign = :left, fontsize = 15, font = "TeX Gyre Heros Bold Makie")
-Label(titlelayout[2, 1], "2000-2017, all age groups", halign = :left, fontsize = 10)
-rowgap!(titlelayout, 0)
-f
+r, f = plot(res, (-2.05), 10^(-5))
 save(joinpath(outp, "distances.pdf"), f)
-StatsPlots.histogram(year(di, 2017).density)
