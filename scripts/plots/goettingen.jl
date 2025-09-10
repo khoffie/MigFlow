6f07062652d04036c27647c54f5610fe4fed0a6e
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-10 16:38:55 +0200

    good plot showing flows from centers

--- scripts/plots/goettingen.jl
+++ scripts/plots/goettingen.jl
@@ -1,19 +1,21 @@
-using CSV, DataFrames, Revise, GeoStats, CairoMakie, StatsBase
+using CSV, DataFrames, Revise, CairoMakie, StatsBase, GeoStats
+
 outp = "/home/konstantin/paper/sections/texdocs/images/"
-includet("../src/analyzegeo.jl")
-includet("plots/utils.jl")
-function agelines!(df, ax, ages, xcol, ycol)
-    for a in ages
-        foo = age(df, a)
+includet("utils.jl")
+
+function addlines!(df, ax, group, xcol, ycol)
+    for g in unique(group)
+        foo = df[group .== g, :]
         lines!(ax, foo[!, xcol], foo[!, ycol], label = a)
     end
 end
 
 
-df = CSV.read("../data/FlowDataGermans.csv", DataFrame);
-districts = CSV.read("../data/districts.csv", DataFrame);
-correct = CSV.read("../data/clean/correct.csv", DataFrame)
-correct = year(correct, years)
+df = CSV.read("../../data/FlowDataGermans.csv", DataFrame);
+df2 = outflux(df, sum, [:todist, :year])
+out = outflux(df2, sum, [:year])
+districts = CSV.read("../../data/districts.csv", DataFrame);
+correct = CSV.read("../../data/clean/correct.csv", DataFrame)
 ages = unique(df.agegroup)
 allyears = unique(df.year)
 
@@ -21,39 +23,43 @@ allyears = unique(df.year)
 ## To get a quick idea we will count the flows that are 5x higher than
 ## the average between 2006-2008
 
-df2 = outflux(df, sum, [:todist, :year])
-dub = df2[df2.year .<= 2005, :]
-ave = outflux(year(df2, 2006.0, 2008.0), mean, [:todist], "average")
-leftjoin!(dub, ave, on = [:fromdist, :todist])
-dub.rel = dub.flows ./ dub.average
-dub.toolarge = dub.rel .>= 5.0
-df3 = sort(combine(groupby(dub, [:fromdist, :year]), :toolarge => sum => :N), :N)
-sort(combine(groupby(df3, :fromdist), :N => sum => :N), :N)
+function toolarge(df, flow_th, avg_th)
+    dub = df2[df2.year .<= 2005, :]
+    ave = outflux(year(df2, 2006.0, 2008.0), mean, [:todist], "average")
+    leftjoin!(dub, ave, on = [:fromdist, :todist])
+    dub.rel = dub.flows ./ dub.average
+    dub.toolarge = dub.rel .>= flow_th
+    dub = dub[dub.average .>= avg_th, :]
+    return dub, sort(combine(groupby(dub, [:fromdist, :year]), :toolarge => sum => :N), :N)
+end
 
+dub, dflarge = toolarge(df2, 5.0, 5.0)
+
+## https://www.bva.bund.de/DE/Services/Buerger/Migration-Integration/Spaetaussiedler/04_Informationen/Grenzdurchgangslager/Grenzdurchgangslager_node.html?utm_source=chatgpt.com
+## Here 9 centers are mentioned, which existed until october 2000:
+## Friedland, Nürnberg, Osnabrück, Bramsche, Hamm, Empfingen, Dranse
+## und Schönberg-Holm. (the source says 9 but only lists 8)
+
+##Friedland in Göttingen, Bramsche in district Osnabrück, Empfingen in
+## Freudenstadt, Dranse in Ostprignitz-Ruppin, Schönberg-Holm in Plön
+
+late_names = ["Göttingen", "Nürnberg" , "Osnabrück Kreis", "Hamm", "Freudenstadt", "Plön"]
+late = DataFrame(name = late_names)
+leftjoin!(late, year(districts, 2017)[!, [:distcode, :name]], on = :name)
+late.distcode = Int.(late.distcode)
 
 f = Figure(size = (600, 400), fontsize = 10);
-ax1 = Axis(f[1, 1],
-           title = "Outflux district Göttingen",
-           xlabel = "Year",
-           ylabel = "Outflux (in thousands)")
-lines!(ax1, allyears, origin(outflux(df, sum, [:year]), 3159).flows ./ 1000)
-ax2 = Axis(f[1, 2],
-           title = "Flows from Göttingen to Unna",
-           xlabel = "Year",
-           ylabel = "Flows (in thousands)")
-lines!(ax2, allyears, destination(origin(df2, :3159), :5978).flows ./ 1000)
-ax3 = Axis(f[1, 3],
-           title = "Flows from Göttingen to Spree-Neiße",
-           xlabel = "Year",
-           ylabel = "Flows (in thousands)")
-lines!(ax3, allyears, destination(origin(df2, :3159), :12071).flows ./ 1000)
-save(joinpath(outp, "goettingen.pdf"), f)
-
-influx(destination(df2, 3159), sum, [:year])
-influx(destination(df2, 5978), sum, [:year])
-
-code(districts, 12071)
-code(districts, 5978)
-
-filter(row -> row.ags_new in x, correct)
-getname(df, x) = unique(df[df.ags_new .== x, :name_old])[1]
+ags = reshape(late.distcode, 2, 3)
+for i in 1:size(ags, 1)
+    for j in 1:size(ags, 2)
+        c = ags[i, j]
+        ax = Axis(f[i, j],
+                  xlabel = "Year",
+                  ylabel = "Outflux (in thousands)",
+                  title = code(late, c).name[1],
+                  xgridvisible = false, ygridvisible = false)
+        df = origin(out, c)
+        lines!(ax, df.year, df.flows ./ 1000)
+    end
+end
+save(joinpath(outp, "late.pdf"), f)
