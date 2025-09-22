7c28322153027624524504256e8f104d5824b85d
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-22 12:24:53 +0200

    extract_params(r::EstimationResult), meta data

--- src/analyzeresults.jl
+++ src/analyzeresults.jl
@@ -32,12 +32,12 @@ function loopstruct(s, f, ages = nothing, years = nothing)
     return res
 end
 
-function extract_params(result)
-    params = result.chn.name_map.parameters
-    df = DataFrame([Symbol(p) => result.chn[p].data[1] for p in params])
-    df.deviance .= getdeviance(result)
-    df.year .= result.chn[:year].data[1]
-    df.age .= recodeage(Int(result.chn[:age].data[1]))
+function extract_params(r::EstimationResult)
+    params = r.chn.name_map.parameters
+    df = DataFrame([Symbol(p) => r.chn[p].data[1] for p in params])
+    df.deviance .= getdeviance(r)
+    df.year .= r.mdl.data.age
+    df.age .= r.mdl.data.year
     first = ["age", "year", "α_raw", "γ_raw", "ϕ_raw", "lp"]
     last = setdiff(names(df), first)
     select!(df, vcat(first, last))
