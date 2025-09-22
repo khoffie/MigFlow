0f66b37083479d21bba96cf11aa6dd7b23aedc18
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-22 12:38:57 +0200

    Age and Year not stored in Chain anymore

--- src/estimation.jl
+++ src/estimation.jl
@@ -14,27 +14,12 @@ end
 
 function estimate(mdl::ModelWrapper; show_plt = true, optim_kwargs = (;))
     maps = runoptim(mdl.mdl, mdl.lb, mdl.ub; optim_kwargs...)
-    out = format_mles(maps)
-    out = add_age_year(out, mdl.data)
-    chn = makechain(out)
+    chn = makechain(maps)
     prd = Turing.returned(mdl.mdl, chn)[1]
     return EstimationResult(chn, mdl, prd, maps)
 end
 
-function add_age_year(out, data)
-    new_vals = vcat(out, [data.age, data.year])
-    new_names = vcat(names(out)[1], ["age", "year"])
-    return NamedArray(new_vals, (new_names,))
-end
-
-function format_mles(mles)
-    out = NamedArray([mles.values.array..., mles.lp, Int(mles.optim_result.retcode)])
-    nms = [string.(names(mles.values)...)..., "lp", "retcode"]
-    setnames!(out, nms, 1)
-    return out
-end
-
-function runoptim(mdl, lb, ub;
+function runoptim(mdl::ModelWrapper;
                   ad = ADTypes.AutoMooncake(),
                   inits = nothing,
                   reltol = nothing,
@@ -44,7 +29,7 @@ function runoptim(mdl, lb, ub;
     attempt = 0
     while attempt < 5
         try
-            mles = Turing.maximum_a_posteriori(mdl; lb = lb, ub = ub,
+            mles = Turing.maximum_a_posteriori(mdl.mdl; lb = mdl.lb, ub = mdl.ub,
                                              adtype = ad,
                                              initial_params = inits,
                                              maxiters = maxiters,
@@ -66,27 +51,8 @@ function runoptim(mdl, lb, ub;
     error("Optimization failed after $attempt attempts")
 end
 
-function makechain(out)
-    coefs = vcat(out[1 : end - 2], recodeage(String(out["age"])), out["year"])
-    coefsnum = Float64.(coefs.array.array)
-    nms = names(out)[1]
-    return Chains(reshape(coefsnum, (1, :, 1)), nms)
-end
-
-function recodeage(a::String)
-    a == "below18" && return 1
-    a == "18-25" && return 2
-    a == "25-30" && return 3
-    a == "30-50" && return 4
-    a == "50-65" && return 5
-    a == "above65" && return 6
-end
-
-function recodeage(i::Int)
-    i == 1 && return "below18"
-    i == 2 && return "18-25"
-    i == 3 && return "25-30"
-    i == 4 && return "30-50"
-    i == 5 && return "50-65"
-    i == 6 && return "above65"
+function makechain(maps::Turing.Optimisation.ModeResult)
+    estims = maps.values.array
+    nms  = names(maps.values)[1]
+    return Chains(reshape(estims, (1, :, 1)), nms)
 end
