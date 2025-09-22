b2c919cacfaae258e79b5631843f4be147a0d8d8
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-22 16:45:54 +0200

    formatting

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -12,10 +12,35 @@ includet("../src/analyzeresults.jl")
 includet("../src/diagplots.jl")
 include("../src/model.jl")
 include("../src/model_helpers.jl")
+include("../src/plotutils.jl")
 
-mdl = norm(load_data("30-50", 2014, 0.1, "../data/";
-                     only_positive = true,
-                     seed = 1234, opf = false),
-           normalize = false, ndc = 16, ngcx = 5);
+shp = GeoIO.load("../data/clean/shapes/districts_ext.shp");
+st = GeoIO.load("../data/clean/shapes/states.shp")
+
+mdl = baseflow(
+    load_data(
+        "30-50", # age group
+        2014, # year
+        0.1, # Fraction of rows to use, e.g. 10%
+        "../data/"; ## path where FlowDataGermans.csv and districts.csv
+        ## are stored
+        only_positive = true, # return only positive flows / drop zero
+        # flows
+        seed = 1234, # Random seed for reproducibility
+        opf = false # depracated, ignore
+    ),
+    normalize = false, ## normalize desirabilities, ## currently only false supported
+    ndc = 16, # number of radial basis centers for density transition function
+    ngcx = 5 # number of radial basis centers for geographical
+             # asymmetries in x direction. y direction is set
+             # automatically
+);
+
 inits = initialize(mdl.data.age, mdl.mdl.args.ndc, mdl.mdl.args.ngcx, mdl.mdl.args.ngcy);
 @time out = estimate(mdl, optim_kwargs = (; show_trace = false, inits = inits));
+## diagnostic plots
+analyze(out)
+## heatmap of density transition function
+plotdtf(out)
+## Map of Germany showing locational asymmetries
+geo, fig = plotgeo(out, shp, st)
