cea4664b745d7b8e85266e3dbc4e3d5d96852bc9
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-23 11:27:49 +0200

    some utility functions moved to src/utils.jl

--- scripts/plots/utils.jl
+++ scripts/plots/utils.jl
@@ -1,12 +1,3 @@
-age(df, age::Vector{AbstractString}) = filter(:agegroup => n -> n ∈ age, df)
-age(df, age::AbstractString) = filter(:agegroup => n -> n == age, df)
-year(df, y::Vector{Int64}) = filter(:year => n -> n ∈ y, df)
-year(df, y::Int64) = filter(:year => n -> n == y, df)
-year(df, xmin::Float64, xmax::Float64) = df[df.year .<= xmax .&& df.year .>= xmin, :]
-origin(df, o::Vector{Int64}) = filter(:fromdist => n -> n ∈ o, df)
-origin(df, o::Int64) = filter(:fromdist => n -> n == o, df)
-destination(df, d) = filter(:todist => n -> n ∈ d, df)
-code(df, c) = filter(:distcode => n -> n ∈ c, df)
 rmreg(df::DataFrame, col) = filter(col => n -> n != 3159, df) ## data issues
 rmreg(df::GeoTable, col) = GeoTable(filter(col => n -> n != 3159, DataFrame(df))) ## data issues
 
