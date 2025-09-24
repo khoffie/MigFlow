374ff8479301364c6109d8d97aa0e12bbc76764f
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-24 12:15:18 +0200

    prettytitly does not change overall layout anymore

--- src/plotutils.jl
+++ src/plotutils.jl
@@ -10,8 +10,8 @@ function prettytitle!(f, title)
     titlelayout = GridLayout(f[0, 1], tellwidth = false)
     Label(titlelayout[1, 1], title, fontsize = 15, font = "TeX Gyre Heros Bold Makie")
     rowgap!(titlelayout, 0)
-    rowgap!(f.layout, 0)
-    colgap!(f.layout, -50)
+    # rowgap!(f.layout, 0)
+    # colgap!(f.layout, -50)
 end
 
 function hideall!(ax)
