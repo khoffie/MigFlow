6150b8415b92ebb043e65116a8bd5125a0ffce43
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-25 12:22:10 +0200

    diagplots in Makie

--- src/analyze.jl
+++ src/analyze.jl
@@ -9,34 +9,71 @@ function analyze(r::EstimationResult)
         A = data.A,
         P = exp.(data.P[data.to]) ## bec log(P) is saved
     );
-    dev = round(deviance(df.flows, df.preds), digits = 2)
-
-    p1 = plotpop(df.flows, df.preds, df.A, df.P)
-
-    p2 = Plots.plot(plotfit(df.flows, df.preds),
-                    title = "Mean deviance: $(dev)")
-
-    if occursin("dist", names(df)[1])
-        p3 = plotdist(df, :preds)
-    else
-        p3 = nothing
-    end
-
+    dev = round(deviance2(df.flows, df.preds), digits = 2)
     net = calc_net_df(df);
-    p4 = Plots.plot(plotnet(net), title = "cor: $(corround(net.nmr, net.nmrp))")
-    pall = Plots.plot([p2, p4, p3, p1]...)
-    display(pall)
-    plts = [p2, p4, p3, p1, pall]
-    return (; df, net, plts)
+    r2nmr = round(cor(net.nmr, net.nmrp)^2, digits = 2)
+
+    fig = Figure(size = (400, 400), fontsize = 10);
+    ax1 = Axis(fig[1, 1],
+               xlabel = L"\log(\hat{y})",
+               ylabel = L"\log(y)",
+               title = "Mean deviance $dev",
+               aspect = DataAspect())
+    plotfit!(ax1, df.flows, df.preds)
+
+    ax2 = Axis(fig[1, 2],
+               xlabel = L"(\hat{i} - \hat{o}) / (\hat{i} + \hat{o})",
+               ylabel = L"(i - o) / (i + o)",
+               title = L"R^2 = %$r2nmr",
+               aspect = DataAspect())
+    Makie.ylims!(ax2, (-1, 1))
+    Makie.xlims!(ax2, (-1, 1))
+    plotnet!(ax2, net)
+
+    ax3 = Axis(fig[2, 1],
+               xlabel = "Distance (km)",
+               ylabel = L"\log(y / \hat{y})")
+    plotdist!(ax3, df.flows, df.preds, df.dist)
+
+    ax4 = Axis(fig[2, 2],
+               xlabel = L"\log(Pop_d \cdot Pop_o)",
+               ylabel = L"\log(y / \hat{y})")
+    plotpop!(ax4, df.flows, df.preds, df.A, df.P)
+    return (; df, net, fig)
+end
+
+function calc_net(df, col)
+    netf = combine(DataFrames.groupby(df, :fromdist), col => sum)
+    rename!(netf, string(col) * "_sum" => :outflux)
+    nett = combine(DataFrames.groupby(df, :todist), col => sum)
+    rename!(nett, string(col) * "_sum" => :influx)
+    net = innerjoin(netf, nett, on = [:fromdist => :todist])
+    net.net = net.influx .- net.outflux
+    net.total = net.influx .+ net.outflux
+    net.nmr = net.net ./ net.total
+    return net
+end
+
+function calc_net_df(df)
+    net = calc_net(df, :flows)
+    netp = calc_net(df, :preds)
+
+    new = names(netp) .* "p"
+    names(netp)
+    rename!(netp, names(netp) .=> new)
+    net = innerjoin(net, netp, on = :fromdist => :fromdistp)
+    return net
+end
+
+subset(x, n) = StatsBase.sample(1:length(x), n)
+
+function extract_coefs(chn::Chains)
+    return vec(chn.value.data)
 end
 
 function extract_coefs(chn::Chains, string::String)
     nms = String.(names(chn))
     return chn.value[occursin.(string, nms)].data
-end
-
-function extract_coefs(chn::Chains)
-    return vec(chn.value.data)
 end
 
 function extract_sample(chn, type = "best")
@@ -47,12 +84,61 @@ function extract_sample(chn, type = "best")
     return chn
 end
 
-function deviance(y, p)
+function plotfit!(ax, flows, preds)
+    idx = subset(flows, 10^3)
+    x = log.(preds)[idx]
+    y = log.(flows)[idx]
+    df = DataFrame(; x, y)
+    Makie.scatter!(ax, df.x, df.y, alpha = .5)
+    diagonal!(ax, df.x, df.y)
+    smoother!(ax, df.x, df.y)
+end
+
+function plotnet!(ax, net)
+    Makie.scatter!(ax, net.nmrp, net.nmr, alpha = .5)
+    diagonal!(ax, net.nmrp, net.nmr)
+    smoother!(ax, net.nmrp, net.nmr)
+end
+
+function plotdist!(ax, flows, preds, dist)
+    idx = subset(flows, 10^3)
+    y = log.(flows ./ preds)[idx]
+    x = dist[idx]
+    df = sort(DataFrame(; x, y), :x)
+    Makie.scatter!(ax, df.x, df.y, alpha = .5)
+    Makie.hlines!(ax, [0], color = :darkred, linewidth = 2)
+    smoother!(ax, df.x, df.y)
+end
+
+function plotpop!(ax, flows, preds, frompop, topop)
+    idx = subset(flows, 10^3)
+    x = (log.(frompop) .+ log.(topop))[idx]
+    y = (log.(flows ./ preds))[idx]
+    df = sort(DataFrame(; x, y), :x)
+    Makie.scatter!(ax, x, y, alpha = .5)
+    Makie.hlines!(ax, [0], color = :darkred, linewidth = 2)
+    smoother!(ax, x, y)
+end
+
+function diagonal!(ax, x, y)
+    xmin, xmax = extrema(x)
+    ymin, ymax = extrema(y)
+    low = min(xmin, ymin)
+    high = max(xmax, ymax)
+    Makie.lines!(ax, [low, high], [low, high],
+          color = :darkred, linewidth = 2)
+end
+
+function smoother!(ax, x, y, col = "red", span = .5)
+    us = range(extrema(x)...; step = .1)
+    vs = Loess.predict(loess(x, y; span = span), us)
+    return Makie.lines!(ax, us, vs, color = col, linewidth = 4)
+end
+
+function deviance2(y, p)
     loss = zeros(length(y))
     for i in eachindex(y)
         loss[i] = y[i] * log(y[i] / p[i]) - (y[i] - p[i])
     end
     return 2mean(loss)
 end
-
-corround(x, y) = round(cor(x, y), digits = 2)
