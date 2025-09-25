0e30879e1064fadf1e1d0defc99a42084f6d9211
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-25 09:28:40 +0200

    typo

--- src/coefplot.jl
+++ src/coefplot.jl
@@ -1,13 +1,14 @@
 function coefplot(r::EstimationResult)
+    df = r.ses
     a, y = getageyear(r)
     fig = Figure(size = (1000, 400), fontsize = 10);
     ax = Axis(fig[1, 1],
               xlabel = "Parameter",
               ylabel = "Estimate +. 2se",
-              xticks = (1:nrow(ses), ses.name),
+              xticks = (1:nrow(df), df.name),
               xgridvisible = false)
-    errorbars!(ax, 1:nrow(ses), ses.coef, 2ses.se)
-    Makie.scatter!(ax, 1:nrow(ses), ses.coef)
+    errorbars!(ax, 1:nrow(df), df.coef, 2df.se)
+    Makie.scatter!(ax, 1:nrow(df), df.coef)
     prettytitle!(fig, "Estimates $a in $y")
     return fig
 end
