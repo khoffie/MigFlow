de59cb976a1132207e209e68b86528831dac7711
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-26 15:30:18 +0200

    nothing really

--- src/coefplot.jl
+++ src/coefplot.jl
@@ -1,14 +1,15 @@
 function coefplot(r::EstimationResult)
     df = r.ses
+    xs = 1:nrow(df)
     a, y = getageyear(r)
     fig = Figure(size = (1000, 400), fontsize = 10);
     ax = Axis(fig[1, 1],
               xlabel = "Parameter",
               ylabel = "Estimate +- 2se",
-              xticks = (1:nrow(df), df.name),
+              xticks = (xs, df.name),
               xgridvisible = false)
-    errorbars!(ax, 1:nrow(df), df.coef, 2df.se)
-    Makie.scatter!(ax, 1:nrow(df), df.coef)
+    errorbars!(ax, xs, df.coef, 2df.se)
+    Makie.scatter!(ax, xs, df.coef)
     prettytitle!(fig, "Estimates $a in $y")
     return fig
 end
