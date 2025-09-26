99f46ebac013e19f194e33d9bbab7de7adec14a1
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-09-26 15:29:37 +0200

    yearmap, plotgeoyears mv to paper repo

--- src/georbf.jl
+++ src/georbf.jl
@@ -9,27 +9,6 @@ function plotgeo(r::EstimationResult, shp::GeoTable, st::GeoTable)
     return geodf, fig
 end
 
-function yearmap(df, shp, st, a, yr, fig, x = 1, y = 1, crange = nothing)
-    df = year(age(df, a), yr)
-    df = joingeometry(df, DataFrame(shp))
-    ax = Axis(fig[x, y], aspect=DataAspect(),
-          title = "$yr")
-    viz!(ax, df.geometry, color = df.geo, colorrange = crange);
-    hideall!(ax)
-    overlay_states(ax, st)
-end
-
-function plotgeoyears(df, col, shp, st, a, years, fig, crange)
-    for i in eachindex(years)
-        pos = grid_position(i, 3)
-        yearmap(df, col, shp, st, a, years[i], fig, pos[1], pos[2], crange)
-    end
-    Colorbar(f[end + 1, :], colorrange = crange, vertical = false, height = 3,
-             label = "Locational Asymmetries", width = Relative(.9))
-    prettytitle!(f, "Locational Asymmetries, $a")
-    resize_to_layout!(f)
-end
-
 function overlay_states(ax, stshp)
     viz!(ax, stshp.geometry, showsegments = true, alpha = 0,
          segmentcolor = :white, segmentsize = 0.5)
