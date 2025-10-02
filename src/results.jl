399b0e057f3ea605f77dae2c005e5f47af1097c7
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-10-02 13:46:23 +0200

    readresults, optionally specify file pattern

--- src/results.jl
+++ src/results.jl
@@ -1,6 +1,7 @@
-function readresults(path = "./output")
+function readresults(path = "./output", pattern = nothing)
     years = Symbol.("y" .* string.(vcat(2000:2002, 2004:2017)))
     files = readdir(path; join = true)
+    files = files[contains.(files, pattern)]
     ## extracting age group
     ages = string.([s[end] for s in split.(files, "/")])
     ages = "age" .* ([s[2] for s in split.(ages, "optim")])
