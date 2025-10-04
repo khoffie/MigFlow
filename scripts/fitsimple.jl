740a39edf68375a33466c38551b42396de6c31d2
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-10-04 14:14:03 +0200

    fitmodels()

--- scripts/fitsimple.jl
+++ scripts/fitsimple.jl
@@ -1,6 +1,5 @@
 using CSV, DataFrames, Turing, Mooncake, StatsBase, Random, Distributions,
-    CategoricalArrays, NamedArrays, LaTeXStrings, Loess, ADTypes, KernelDensity,
-    IterTools, GeoStats, GeoIO, CairoMakie, DynamicPPL, Serialization
+    CategoricalArrays, NamedArrays, ADTypes, DynamicPPL, Serialization
 
 include("../src/estimation.jl")
 include("../src/loadgermdata.jl")
@@ -18,17 +17,22 @@ function defmodel(m, age, year)
     return m(load_data(age, year, 1.0, "../data/"; only_positive = true));
 end
 
-ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
-years = vcat(2000:2002, 2004:2017)
-
-for  m in [fundamental, norm, gravity]
-    for a in ages
-        name = "$(m)$(a)"
-        results = Vector{EstimationResult}(undef, length(years))
-        Threads.@threads for i in eachindex(years)
-            results[i] = estimate(defmodel(m, a, years[i]))
+function fitmodels(models, ages, years)
+    for  m in models
+        for a in ages
+            name = "$(m)$(a)"
+            results = Vector{EstimationResult}(undef, length(years))
+            Threads.@threads for i in eachindex(years)
+                results[i] = estimate(defmodel(m, a, years[i]))
+            end
+            serialize(joinpath(outp, name), results)
+            println("$name done")
         end
-        serialize(joinpath(outp, name), results)
-        println("$name done")
     end
 end
+
+ages = ["below18", "18-25", "25-30", "30-50", "50-65", "above65"]
+years = vcat(2000:2002, 2004:2017)
+models =  [fundamental, norm, gravity]
+
+fitmodels(models, ages, years)
