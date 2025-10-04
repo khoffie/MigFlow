9886617609d5803a9fa1aa7fc507b4a8a1d42425
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-10-04 17:43:31 +0200

    cleanup

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -20,37 +20,4 @@ include("../models/gravity.jl")
 shp = GeoIO.load("../data/clean/shapes/districts_ext.shp");
 st = GeoIO.load("../data/clean/shapes/states.shp");
 
-mdl = baseflow(
-    load_data(
-        "25-30", # age group
-        2002, # year
-        1.0, # Fraction of rows to use, e.g. 10%
-        "../data/"; ## path of FlowDataGermans.csv and districts.csv
-        only_positive = true, # use only positive flows / drop zero flows
-        seed = 1234, # for reproducibility when sampling rows
-    ),
-    ndc = 16, # number of radial basis centers for density transition function
-    ngcx = 5 # number of radial basis centers for geographical
-             # asymmetries in x direction. y direction is set
-             # automatically
-);
-
-out = @time estimate(mdl);
-df, net, quick, figs = analyze(out)
-m, p1 = plotdtf(out)
-g, p2 = plotgeo(out, shp, st)
-figs
-p1
-p2
-
-p3  = plotcovcor(out)
-
-fig2 = Figure(size = (1000, 400));
-ax = Axis(fig2[1, 1],
-          xticks = (1:53, nms[2:end]),
-          xgridvisible = false, ygridvisible = false,
-          title = "correlations with alpha",
-          xticklabelrotation = -π/4)
-barplot!(ax, 1:53, cormat[1, 2 : end].array)
-fig2
-cormat
+results, ages = readresults(["fundamental", "norm", "gravity"]);
