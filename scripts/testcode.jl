fd0bcba8a1a8cdd0dce23fce0e448b95342e3f97
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-10-12 09:43:11 +0200

    trying different truncated implementations

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -13,12 +13,26 @@ include("../src/modelutils.jl")
 include("../src/plotutils.jl")
 include("../src/utils.jl")
 include("../src/coefplot.jl")
+include("../src/plotcovcor.jl")
 
 include("../models/baseflow.jl")
+include("../models/baseflowtruncated.jl")
 include("../models/fundamental.jl")
 include("../models/gravity.jl")
+include("../models/TruncatedPoisson.jl")
 
 shp = GeoIO.load("../data/clean/shapes/districts_ext.shp");
 st = GeoIO.load("../data/clean/shapes/states.shp");
 
-results, ages = readresults(["fundamental", "norm", "gravity"]);
+results, ages = readresults(["fundamental", "norm", "gravity", "baseflow"]);
+
+mdl = baseflowtruncated(load_data("18-25", 2010, 1.0,
+                                  "../data"; only_positive = true);
+                        ndc = 16, ngcx = 5);
+
+
+mdl1 = truncated(load_data("18-25", 2010, 1.0, "../data"; only_positive = true); type = 1);
+mdl2 = truncated(load_data("18-25", 2010, 1.0, "../data"; only_positive = true); type = 2);
+mdl3 = truncated(load_data("18-25", 2010, 1.0, "../data"; only_positive = true); type = 3);
+
+out = @time estimate(mdl; optim_kwargs = (inits = r.ses.coef,));
