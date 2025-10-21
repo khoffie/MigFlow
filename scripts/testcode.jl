0a0a17b2a03ce1a5b4fbd9f2db341ef2cbf4b225
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-10-21 09:54:34 +0200

    taken gradients

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -22,22 +22,72 @@ shp = GeoIO.load("../data/clean/shapes/districts_ext.shp");
 st = GeoIO.load("../data/clean/shapes/states.shp");
 
 results, ages = readresults(["fundamental", "norm", "gravity"]);
-
-mdl = baseflownormalized(load_data("18-25", 2010, 1.0, "../data/"; only_positive = true);
+results, ages = readresults(["baseflow"]);
+results.baseflow.age_18to25.y2010.mdl.meta
+mdl = baseflownormalized(load_data("18-25", 2017, 1.0, "../data/"; only_positive = true);
                          ndc = 16, ngcx = 5)
-## mdl = fundamental(load_data("18-25", 2010, 1.0, "../data/"; only_positive = true))
+# mdl = fundamental(load_data("18-25", 2010, 1.0, "../data/"; only_positive = true))
 
-@profilehtml estimate(mdl)
+out = @time estimate(mdl; optim_kwargs = (reltol = 1e-2, ))
+ana = analyze(out)
+ana.fig
+m, p = plotdtf(out)
+p
+m, p = plotgeo(out, shp, st)
+p
 
+out.mdl.meta
 # ana = analyze(out)
 # m, p = plotdtf(out)
 # m, p = plotgeo(out, shp, st)
 # p
 # ana.fig
-
-
+out2 = @time estimate(mdl; optim_kwargs = (; maxtime = 100, ))
+out2.mdl.meta
 # results, ages = readresults(["baseflownorm"]);
 # r = results.baseflownormalized.agealized_below18.y2010
 # m, p = plotdtf(r)
 # m, p = plotgeo(r, shp, st)
 # p
+
+
+# struct TruncatedPoisson{T<:Real} <: DiscreteUnivariateDistribution λ::T end
+
+# function Distributions.logpdf(d::TruncatedPoisson, k::Real)
+#     if k < 1
+#         return -Inf
+#     else
+#         λ = d.λ
+#         ## return k * log(λ) - λ - logfactorial(k) - log1mexp(-λ)
+#         return logpdf(Poisson(λ), k) - log(1 - exp(-λ))
+#     end
+# end
+
+# function Distributions.rand(rng::AbstractRNG, d::TruncatedPoisson)
+#     k = 0
+#     while k < 1
+#         k = rand(rng, Poisson(d.λ))
+#     end
+#     return k
+# end
+
+
+# lvals = 0.01:0.02:1.0
+
+# fig = Figure(size = (400, 400));
+# ax = Axis(fig[1, 1])
+
+# plot!(ax, lvals, Distributions.logpdf.(TruncatedPoisson.(lvals), 1.0))
+# plot!(ax, lvals, Distributions.logpdf.(Poisson.(lvals), 1.0))
+# fig
+
+# using DifferentiationInterface
+
+# f(x) = Distributions.logpdf(TruncatedPoisson(x), 1)
+# backend = AutoMooncake(; config=nothing)
+# prep = prepare_gradient(f, backend, .1)
+# gradient(f, prep, backend, 10.0)
+
+
+# x = 0
+# x / (1 - exp(-x))
