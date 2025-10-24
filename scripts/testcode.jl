8420270f6b033633b614c2d50f7187154d300539
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-10-24 15:38:23 +0200

    cleanup

--- scripts/testcode.jl
+++ scripts/testcode.jl
@@ -21,60 +21,13 @@ include("../models/baseflow.jl")
 include("../models/gravity.jl")
 include("../models/TruncatedPoisson.jl")
 
-di = year(CSV.read("../data/districts.csv", DataFrame), 2017)
-di.lc = lc(di.distcode)
-shp = GeoIO.load("../data/clean/shapes/districts_ext.shp");
-st = GeoIO.load("../data/clean/shapes/states.shp");
-
-results, ages = readresults(["baseflow"], "./output");
-results, ages = readresults(["baseflow"], "./output_trunc_norm/");
-
-r = results.baseflow.age_18to25.y2017;
-r = results.baseflow_truncated_normalized.age_below18_true_true.y2017;
-r.mdl.meta
-rana = analyze(r);
-rm, rp = plotdtf(r);
-rm2, rp2 = plotgeo(r, shp, st);
-
 
 mdl = baseflow(load_data("18-25", 2017, 0.1, "../data/"; only_positive = true);
                   ndc = 25, ngcx = 5, trunc = false, norm = false);
-out = @time estimate(mdl);
-inits = vcat(out.ses.coef[1], 1.0, out.ses.coef[2:end])
-mdl = baseflow(load_data("18-25", 2017, 1.0, "../data/"; only_positive = true);
-                  ndc = 25, ngcx = 5, trunc = true, norm = true);
-out = @time estimate(mdl, optim_kwargs = (; inits = inits, maxiters = 1, maxtime = 600));
+@time maximum_a_posteriori(mdl.mdl; maxtime = 10)
 
+out = @time estimate(mdl; optim_kwargs = (; maxtime = 100));
 out.mdl.meta
-ana = analyze(out);
-m, p = plotdtf(out);
-m2, p2 = plotgeo(out, shp, st);
-
-p
-rp
-rp2
-p2
-
-fig = Figure((size = (800, 400)));
-ax1 = Axis(fig[1, 1])
-scatter!(ax1, df1.asymp, df1.asym)
-smoother!(ax1, df1.asymp, df1.asym)
-
-ax2 = Axis(fig[2, 1])
-scatter!(ax2, df2.asymp, df2.asym)
-smoother!(ax2, df2.asymp, df2.asym)
-
-fig
-sort(df1, :asym)
-sort(df2, :asym)
-
-
-ana.df
-rana.df
-
-scatter(df1.asymp, df2.asymp, color = df2.)
-
-net = ana.net
 
 net = vcat(ana.net, rana.net)
 crange = extrema(vcat(net.asym, net.asymp))
