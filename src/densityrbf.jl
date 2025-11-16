9409842f63df06c45b600afe87333227a82a76ff
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-11-16 18:18:51 +0100

    dtfplots actual density ticks

--- src/densityrbf.jl
+++ src/densityrbf.jl
@@ -3,22 +3,42 @@ function plotdtf(r::EstimationResult, crange = nothing,
                  x = 1, y = 1, legend = true)
     m, a, yr = dtfmat(r)
     if isnothing(crange); crange = extrema(m); end
-    plotdtf(m, a, yr, crange, fig, x, y, legend)
+    plotdtf(m, a, yr, crange, fig, x, y, nothing, legend)
+    return m, fig
+end
+
+function plotdtf(r::EstimationResult, districts::DataFrame, crange = nothing,
+                 fig = Figure(size=(400, 400), fontsize = 10),
+                 x = 1, y = 1, legend = true)
+    m, a, yr = dtfmat(r)
+    if isnothing(crange); crange = extrema(m); end
+    plotdtf(m, a, yr, crange, fig, x, y, districts, legend)
     return m, fig
 end
 
 function plotdtf(m::LinearAlgebra.Adjoint{Float64, Matrix{Float64}},
-                 age::String, year::Int, crange, fig, x, y, legend = true)
+                 a::String, yr::Int, crange, fig, x, y,
+                 districts = nothing, legend = true)
+    if !isnothing(districts)
+        di = year(districts, yr)
+        l, h = Int.(round.(extrema(di.density), digits = 0))
+        l = string(l); h = string(h)
+        xt = ([1, 100], [l, h])
+        yt = ([1, 100], [l, h])
+    else
+        xt = ([1, 100], ["low", "high"])
+        yt = ([1, 100], ["low", "high"])
+    end
     ax = Axis(fig[x, y],
               xlabel = "Origin density",
               ylabel = "Destination density",
-              xticks = ([1, 100], ["low", "high"]),
-              yticks = ([1, 100], ["low", "high"]),
-              title = string(year), aspect = DataAspect())
+              xticks = xt,
+              yticks = yt,
+              title = string(yr), aspect = DataAspect())
     hm = Makie.heatmap!(ax, m, colorrange = crange)
     if legend
         Colorbar(fig[:, y + 1], hm, vertical = true, width = 5)
-        prettytitle!(fig, "Density Transition Function, $age")
+        prettytitle!(fig, "Density Transition Function, $a")
     end
 end
 
