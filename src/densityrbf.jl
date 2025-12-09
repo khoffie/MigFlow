36c5f1947ae18fac4e8095e6ca596b22c3f3f323
Konstantin Hoffie <konstantin.hoffie@web.de>
2025-12-09 17:09:25 +0100

    hand chosen dtf tick labels

--- src/densityrbf.jl
+++ src/densityrbf.jl
@@ -1,44 +1,27 @@
-function plotdtf(r::EstimationResult, districts, crange = nothing,
-                 fig = Figure(size=(400, 400), fontsize = 10),
+function plotdtf(r::EstimationResult, crange = nothing, fig = genfig((10, 10)),
                  x = 1, y = 1, legend = true)
     m, a, yr = dtfmat(r)
     if isnothing(crange); crange = calc_crange(m); end
-    plotdtf(m, a, yr, crange, fig, x, y, districts, legend)
-    return m, fig
-end
-
-function plotdtf(r::EstimationResult, districts::DataFrame, crange = nothing,
-                 fig = Figure(size=(400, 400), fontsize = 10),
-                 x = 1, y = 1, legend = true)
-    m, a, yr = dtfmat(r)
-    if isnothing(crange); crange = calc_crange(m); end
-    plotdtf(m, a, yr, crange, fig, x, y, districts, legend)
+    plotdtf(m, a, yr, crange, fig, x, y, legend)
     return m, fig
 end
 
 function plotdtf(m::LinearAlgebra.Adjoint{Float64, Matrix{Float64}},
-                 a::String, yr::Int, crange, fig, x, y,
-                 districts = nothing, legend = true)
-    if !isnothing(districts)
-        di = year(districts, yr)
-        qs = [.01, .25, .5, .75, .9, 1]
-        xs = string.(Int.(round.(quantile(di.density, qs), digits = 0)))
-        xt = (100qs, xs)
-        yt = (100qs, xs)
-    else
-        xt = ([1, 100], ["low", "high"])
-        yt = ([1, 100], ["low", "high"])
-    end
+                 a::String, yr::Int, crange, fig, x, y, legend = true)
+    qs = [.01, .25, .5, .75, .9, 1]
+    ## from round.(quantile(districts.density, [0.0, .25, .5, .75, 0.9, 1.0]), digits = 0) in 2017
+    ys = string.([35, 115, 200, 670, 1500, 4700])
+    tks = (100qs, ys)
+
     ax = Axis(fig[x, y],
               xlabel = L"\textrm{Origin} $\mathrm{pop} / \mathrm{km}^2$",
               ylabel = L"\textrm{Destination} $\mathrm{pop} / \mathrm{km}^2$",
-              xticks = xt,
-              yticks = yt,
+              xticks = tks, yticks = tks,
               title = string(yr), aspect = DataAspect(),
               xticklabelrotation = 1.0)
     hm = Makie.heatmap!(ax, m, colorrange = crange, colormap = :roma)
     if legend
-        Colorbar(fig[:, y + 1], hm, vertical = true, width = 5)
+        Colorbar(fig[:, y + 1], hm)
         prettytitle!(fig, "Density Transition Function, $a")
     end
 end
@@ -50,9 +33,8 @@ function dtfmat(r::EstimationResult)
     cx = data.cx
     cy = data.cy
     scale = data.rbf_scale
-    R = data.R
-    Rmin, Rmax = data.Rmin, data.Rmax
     anchor = data.anchor
+    Rmin = -1.0; Rmax = 1.0
     vals = range(Rmin, Rmax, 100)
     m = [interp_anchor(Rfrom, Rto, coefmat(coefs ./ 10), cx, cy, scale, anchor)
          for Rto in vals, Rfrom in vals]'
